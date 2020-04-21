e497428f7179f2f0e834ee2aebe54a25c1642924
zeejay09 <zeejaybelamide@gmail.com>
2020-04-21 17:29:43 +0800

    removes container-fluid class on header

--- src/components/Navigation/NavigationHeader.js
+++ src/components/Navigation/NavigationHeader.js
@@ -10,23 +10,20 @@ class NavigationHeader extends Component {
     render() {
         return (
             <header>
-                <div className="container-fluid">
-                    <Navbar bg="dark" expand="lg">
-                        <NavLink href={'/'} className="navbar-brand text-white">Scorm Generator</NavLink>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-                            <ul className="navbar-nav">
-                                <li className="nav-item">
-                                    <NavLink href='/about' className="nav-link text-white">About</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink href='/contact' className="nav-link text-white">Contact</NavLink>
-                                </li>
-                            </ul>
-                        </Navbar.Collapse>
-                    </Navbar>
-                    
-                </div>
+                <Navbar bg="dark" expand="lg">
+                    <NavLink href={'/'} className="navbar-brand text-white">Scorm Generator</NavLink>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink href='/about' className="nav-link text-white">About</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink href='/contact' className="nav-link text-white">Contact</NavLink>
+                            </li>
+                        </ul>
+                    </Navbar.Collapse>
+                </Navbar>
             </header>
         )
     }
