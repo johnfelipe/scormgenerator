59d019fe49a12c1db982e72cc7010aacb9b403b4
zeejay09 <zeejaybelamide@gmail.com>
2020-05-04 09:32:00 +0800

    adds grid system and adds input type file for logo

--- src/components/Generator/Main.js
+++ src/components/Generator/Main.js
@@ -6,11 +6,41 @@ import '../../assets/bootstrap/css/bootstrap.min.css';
 
 class Main extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            courseTitle: '',
+        };
+    }
+
     render() {
         return (
             <div className="container-fluid">
-                <div className="text-center">
-                    <h3>Try it out!</h3>
+                <div id="generator-container">
+                    <div className="row">
+                        <div className="col-md-12 text-center">
+                            <h3>Try it out!</h3>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-9">
+                            <input
+                                id="courseTitle"
+                                name="courseTitle"
+                                type="text"
+                                className="form-control"
+                                placeholder="Type course name here . . ."
+                            />
+                        </div>
+                        <div className="col-md-3">
+                            <input
+                                id="myfile"
+                                name="myfile"
+                                type="file"
+                                className="form-control"
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         )
