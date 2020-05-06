120527199faa7f16158059cca17a1a368ad3999c
zeejay09 <zeejaybelamide@gmail.com>
2020-05-06 14:01:32 +0800

    wrap all fields in a form using formik

--- src/components/Generator/Main.js
+++ src/components/Generator/Main.js
@@ -3,6 +3,8 @@ import { Accordion, Card, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWindowClose } from '@fortawesome/free-solid-svg-icons';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { Formik } from "formik";
+import * as Yup from 'yup';
 
 // styling
 import '../../css/styles.css';
@@ -189,119 +191,144 @@ class Main extends Component {
         return (
             <div className="container-fluid">
                 <div id="generator-container">
-                    <div className="row">
-                        <div className="col-md-12 text-center">
-                            <h3>Try it out!</h3>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-9">
-                            <TextInput 
-                                placeholder="Type course name here . . ."
-                            />
-                        </div>
-                        <div className="col-md-3">
-                            <FileInput />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-8 mt-2">
-                            <SelectInput />
-                        </div>
-                        <div className="col-md-4 mt-2">
-                            <div className="float-right mt-2">
-                                <CheckBoxInput />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-4 mt-2">
-                            <ResourcesUpload />
-                        </div>
-                        <div className="col-md-4 mt-2">
-                            <TranscriptUpload />
-                        </div>
-                        <div className="col-md-4 mt-2">
-                            <AddGlossary />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-12 mt-2">
-                            <div id="lesson-container">
-                                <div
-                                    className="lesson-container"
-                                >
-                                    {this.state.lessons.map((item, index) => (
-                                        <Accordion
-                                            key={index}
-                                        >
-                                            <Card>
-                                                <Card.Header>
-                                                    <Accordion.Toggle as={Button} variant="link" eventKey="0" className="pr-0">
-                                                        <span onClick={() => this.onLessonClickListener(index)}>{item.lessonName}</span>
-                                                    </Accordion.Toggle>
-                                                    <LessonHandler editLessonNameChange={this.editLessonNameHandler} action="edit" currentLessonName={item.lessonName} id={index}/>
+                <Formik
+                        initialValues={{ 
+                        
+                        }}
 
-                                                    <button className="btn btn-danger float-right lesson-item-remove-btn" title="Remove" onClick={() => this.removeLesson(index)}><FontAwesomeIcon icon={faWindowClose} /></button>
-                                                </Card.Header>
-                                                <Accordion.Collapse eventKey="0">
-                                                    <Card.Body>
-                                                        <SlideHandler addSlideChange={this.addSlideHandler} action="add" id={index}/>
-                                                        {this.state.lessons[index].slides ?
-                                                            <DragDropContext onDragEnd={this.onDragEnd}>
-                                                                <Droppable droppableId="slides">
-                                                                    {(provided) => (
-                                                                        <div
-                                                                            className="slide-container mt-3"
-                                                                            ref={provided.innerRef}
-                                                                        >
-                                                                            {this.state.lessons[index].slides.map((item, index) => (
-                                                                                <Draggable
-                                                                                    key={index}
-                                                                                    draggableId={'' + index}
-                                                                                    index={index}>
+                        onSubmit={values => {
+                        
+                        }}
+                    >
+                    {props => {
+                            const {
+                            values,
+                            touched,
+                            errors,
+                            isSubmitting,
+                            handleChange,
+                            handleBlur,
+                            handleSubmit,
+                            } = props;
+                            return (
+                                <form onSubmit={handleSubmit}>
+                                    <div className="row">
+                                        <div className="col-md-12 text-center">
+                                            <h3>Try it out!</h3>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-9">
+                                            <TextInput 
+                                                placeholder="Type course name here . . ."
+                                            />
+                                        </div>
+                                        <div className="col-md-3">
+                                            <FileInput />
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-8 mt-2">
+                                            <SelectInput />
+                                        </div>
+                                        <div className="col-md-4 mt-2">
+                                            <div className="float-right mt-2">
+                                                <CheckBoxInput />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-4 mt-2">
+                                            <ResourcesUpload />
+                                        </div>
+                                        <div className="col-md-4 mt-2">
+                                            <TranscriptUpload />
+                                        </div>
+                                        <div className="col-md-4 mt-2">
+                                            <AddGlossary />
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-12 mt-2">
+                                            <div id="lesson-container">
+                                                <div
+                                                    className="lesson-container"
+                                                >
+                                                    {this.state.lessons.map((item, index) => (
+                                                        <Accordion
+                                                            key={index}
+                                                        >
+                                                            <Card>
+                                                                <Card.Header>
+                                                                    <Accordion.Toggle as={Button} variant="link" eventKey="0" className="pr-0">
+                                                                        <span onClick={() => this.onLessonClickListener(index)}>{item.lessonName}</span>
+                                                                    </Accordion.Toggle>
+                                                                    <LessonHandler editLessonNameChange={this.editLessonNameHandler} action="edit" currentLessonName={item.lessonName} id={index}/>
+
+                                                                    <button className="btn btn-danger float-right lesson-item-remove-btn" title="Remove" onClick={() => this.removeLesson(index)}><FontAwesomeIcon icon={faWindowClose} /></button>
+                                                                </Card.Header>
+                                                                <Accordion.Collapse eventKey="0">
+                                                                    <Card.Body>
+                                                                        <SlideHandler addSlideChange={this.addSlideHandler} action="add" id={index}/>
+                                                                        {this.state.lessons[index].slides ?
+                                                                            <DragDropContext onDragEnd={this.onDragEnd}>
+                                                                                <Droppable droppableId="slides">
                                                                                     {(provided) => (
                                                                                         <div
-                                                                                            className="slide-item"
+                                                                                            className="slide-container mt-3"
                                                                                             ref={provided.innerRef}
-                                                                                            {...provided.draggableProps}
-                                                                                            {...provided.dragHandleProps}
                                                                                         >
-                                                                                            <span class="btn pr-1">{item.slideName}</span>
-                                                                                            <SlideHandler editSlideChange={this.editSlideHandler} currentSlideName={item.slideName} action="edit" id={index}/>
+                                                                                            {this.state.lessons[index].slides.map((item, index) => (
+                                                                                                <Draggable
+                                                                                                    key={index}
+                                                                                                    draggableId={'' + index}
+                                                                                                    index={index}>
+                                                                                                    {(provided) => (
+                                                                                                        <div
+                                                                                                            className="slide-item"
+                                                                                                            ref={provided.innerRef}
+                                                                                                            {...provided.draggableProps}
+                                                                                                            {...provided.dragHandleProps}
+                                                                                                        >
+                                                                                                            <span class="btn pr-1">{item.slideName}</span>
+                                                                                                            <SlideHandler editSlideChange={this.editSlideHandler} currentSlideName={item.slideName} action="edit" id={index}/>
 
-                                                                                            <button className="btn btn-danger float-right lesson-item-remove-btn" title="Remove" onClick={() => this.removeSlide(index)}><FontAwesomeIcon icon={faWindowClose} /></button>
+                                                                                                            <button className="btn btn-danger float-right lesson-item-remove-btn" title="Remove" onClick={() => this.removeSlide(index)}><FontAwesomeIcon icon={faWindowClose} /></button>
+                                                                                                        </div>
+                                                                                                    )}
+                                                                                                </Draggable>
+                                                                                            ))}
+                                                                                            {provided.placeholder}
                                                                                         </div>
                                                                                     )}
-                                                                                </Draggable>
-                                                                            ))}
-                                                                            {provided.placeholder}
-                                                                        </div>
-                                                                    )}
-                                                                </Droppable>
-                                                            </DragDropContext>
-                                                            :
-                                                            <div className="mt-2">No slide added yet.</div>
-                                                        }
-                                                    </Card.Body>
-                                                </Accordion.Collapse>
-                                            </Card>
-                                        </Accordion>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-6 mt-2">
-                            <LessonHandler addLessonNameChange={this.addLessonNameHandler} action="add"/>
-                        </div>
-                        <div className="col-md-6 mt-2">
-                            <div id="save-btn-container" className="float-right">
-                                <button type="button" className="btn btn-success">Save</button>
-                            </div>
-                        </div>
-                    </div>
+                                                                                </Droppable>
+                                                                            </DragDropContext>
+                                                                            :
+                                                                            <div className="mt-2">No slide added yet.</div>
+                                                                        }
+                                                                    </Card.Body>
+                                                                </Accordion.Collapse>
+                                                            </Card>
+                                                        </Accordion>
+                                                    ))}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-6 mt-2">
+                                            <LessonHandler addLessonNameChange={this.addLessonNameHandler} action="add"/>
+                                        </div>
+                                        <div className="col-md-6 mt-2">
+                                            <div id="save-btn-container" className="float-right">
+                                                <button type="submit" className="btn btn-success" disabled={isSubmitting}>Save</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            );
+                        }}
+                    </Formik>
                 </div>
             </div>
         )
