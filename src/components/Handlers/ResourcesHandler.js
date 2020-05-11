9a3453d0be66e94182e4e21bf9d0f19b0470a274
zeejay09 <zeejaybelamide@gmail.com>
2020-05-11 15:12:49 +0800

    makes the input file in resources dynamic

--- src/components/Handlers/ResourcesHandler.js
+++ src/components/Handlers/ResourcesHandler.js
@@ -9,13 +9,21 @@ class ResourcesHandler extends Component {
         this.state = {
             modalShow: false,
             inputCounter: 1,
+            inputObject: [
+                {name: 'resourceFile1', top: 0}
+            ],
+            top: 45,
         };
         this.setModalShow = this.setModalShow.bind(this);
         this.onSave = this.onSave.bind(this);
+        this.addInput = this.addInput.bind(this);
+        this.getInitialValues = this.getInitialValues.bind(this);
     }
 
     componentDidUpdate = () => {
         console.log(this.props.resourceFilesData);
+        console.log(this.getInitialValues(this.state.inputObject));
+        console.log(this.state.inputObject);
     }
 
     setModalShow = (value) => {
@@ -24,6 +32,38 @@ class ResourcesHandler extends Component {
         });
     }
 
+    addInput = () => {
+        let currentCount = this.state.inputCounter;
+        currentCount += 1;
+
+        if (!(currentCount === 6)) {
+            const inputObj = { name: 'resourceFile' + currentCount, top: this.state.inputObject[currentCount - 2].top + this.state.top };
+
+            this.setState({
+                inputCounter: currentCount,
+                inputObject: [...this.state.inputObject, inputObj],
+            });
+        } else {
+            alert("Already limit!");
+        }
+    }
+
+    getInitialValues = (inputs) => {
+        //declare an empty initialValues object
+        const initialValues = {};
+        //loop loop over fields array
+        //if prop does not exit in the initialValues object,
+        // pluck off the name and value props and add it to the initialValues object;
+        inputs.forEach((field, index) => {
+          if(!initialValues[field.name]) {
+            initialValues[field.name] = this.props.resourceFilesData[field.name] ? this.props.resourceFilesData[field.name] : '';
+          }
+        });
+    
+        //return initialValues object
+        return initialValues;
+      }
+
     onSave = (object) => {
         this.props.resourceFilesHandler(object);
 
@@ -31,6 +71,8 @@ class ResourcesHandler extends Component {
     }
 
     render() {
+        // const currentCount = this.state.inputCounter;
+        const initialValues = this.getInitialValues(this.state.inputObject);
         const resourcesModal = (
             <Modal
                 show={this.state.modalShow}
@@ -47,16 +89,11 @@ class ResourcesHandler extends Component {
                 </Modal.Header>
                 <Modal.Body>
                     <Formik
-                        initialValues={{ 
-                            resourceFile1: this.props.resourceFilesData.resourceFile1 ? this.props.resourceFilesData.resourceFile1 : '',
-                            resourceFile2: this.props.resourceFilesData.resourceFile2 ? this.props.resourceFilesData.resourceFile2 : '',
-                            resourceFile3: this.props.resourceFilesData.resourceFile3 ? this.props.resourceFilesData.resourceFile3 : '',
-                            resourceFile4: this.props.resourceFilesData.resourceFile4 ? this.props.resourceFilesData.resourceFile4 : '',
-                            resourceFile5: this.props.resourceFilesData.resourceFile5 ? this.props.resourceFilesData.resourceFile5 : '',
-                        }}
+                        initialValues= {initialValues}
 
                         onSubmit={values => {
                             this.onSave(values);
+                            console.log(values);
                         }}
                     >
                         {props => {
@@ -69,9 +106,23 @@ class ResourcesHandler extends Component {
                             } = props;
                             return (
                                 <div>
-                                    <button type="button" className="btn btn-success" onClick={() => this.setModalShow(true)}>Add File</button>
+                                    <button type="button" className="btn btn-success" onClick={this.addInput}>Add File</button>
                                     <form onSubmit={handleSubmit}>
-                                        <input
+                                        {this.state.inputObject.map((input, index) => (
+                                            <div key={input.name}>
+                                                <input
+                                                    id={input.name}
+                                                    name={input.name}
+                                                    type="file"
+                                                    className="form-control custom-file-input"
+                                                    onChange={(event) => {setFieldValue(input.name, event.currentTarget.files[0]);}}
+                                                    onBlur={handleBlur}
+                                                    accept="image/x-png,image/gif,image/jpeg"
+                                                />
+                                                <label htmlFor={input.name} style={{top: input.top}} className={input.name + "-input-label custom-file-label"} id="custom-form-label"> { values[input.name] ? values[input.name].name : <span>Choose file</span> }</label>
+                                            </div>
+                                        ))}
+                                        {/* <input
                                             id="resourceFile1"
                                             name="resourceFile1"
                                             type="file"
@@ -80,7 +131,7 @@ class ResourcesHandler extends Component {
                                             onBlur={handleBlur}
                                             accept="image/x-png,image/gif,image/jpeg"
                                         />
-                                        <label htmlFor="resourceFile1" className="resourceFile1-input-label custom-file-label" id="custom-form-label"> { values.resourceFile1 ? values.resourceFile1.name : <span>Choose file</span> }</label>
+                                        <label htmlFor="resourceFile1" className="resourceFile1-input-label custom-file-label" id="custom-form-label"> { values.resourceFile1 ? values.resourceFile1.name : <span>Choose file</span> }</label> */}
                                         {/* <input
                                             id="resourceFile2"
                                             name="resourceFile2"
