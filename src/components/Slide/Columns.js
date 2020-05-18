3ae5e3e2cb2ae07ca161b6550a47c2073cda258c
zeejay09 <zeejaybelamide@gmail.com>
2020-05-18 13:36:29 +0800

    converts react class component to react hook component

--- src/components/Slide/Columns.js
+++ src/components/Slide/Columns.js
@@ -1,50 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Accordion, Card } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faTrash, faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import ContentEditable from 'react-contenteditable';
 
-class Columns extends Component {
-    render() {
-        return (
-            <Accordion key={'accordion-column-' + this.props.columnIndex} className="mb-2">
-                <Card>
-                    <Accordion.Toggle as={Card.Header} eventKey="0" className="section-header p-2">
-                        <ContentEditable
-                            html={this.props.name}
-                            onChange={(event) => this.handleContentEditable(event, this.props.columnIndex)}
-                            className="content-editable d-inline"
-                        />
-                        <button type="button" className="float-right column-item-remove-btn btn btn-link p-0" title="Remove" onClick={() => this.props.deleteColumn(this.props.columnIndex)}><FontAwesomeIcon icon={faTrash}/></button>
-                    </Accordion.Toggle>
-                    <Accordion.Collapse eventKey="0" className="collapsible-body pb-3">
-                        <Card.Body className="section-body">
-                            <ul className="sg-column-layout">
-                                {this.props.columnSizes.map((item, sizeIndex) => (
-                                    this.props.column[this.props.columnIndex].active === sizeIndex ?
-                                        <li key={sizeIndex} className="sg-active">
-                                            {this.props.columnSizes[sizeIndex].items.map((item, index) => (
-                                                <span key={index} className={item.class}>
-                                                    {item.size}
-                                                </span>
-                                            ))}
-                                        </li>
-                                    :
-                                        <li key={sizeIndex} onClick={() => this.props.handleSizeActive(this.props.columnIndex, sizeIndex, item.id)}>
-                                            {this.props.columnSizes[sizeIndex].items.map((item, index) =>(
-                                                <span key={index} className={item.class}>
-                                                    {item.size}
-                                                </span>
-                                            ))}
-                                        </li>
-                                ))}
-                            </ul>
-                        </Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-            </Accordion>
-        )
-    }
+const Columns = props => {
+    return (
+        <Accordion key={'accordion-column-' + props.columnIndex} className="mb-2">
+            <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="0" className="section-header p-2">
+                    <ContentEditable
+                        html={props.name}
+                        onChange={(event) => this.handleContentEditable(event, props.columnIndex)}
+                        className="content-editable d-inline"
+                    />
+                    <button type="button" className="float-right column-item-remove-btn btn btn-link p-0" title="Remove" onClick={() => props.deleteColumn(props.columnIndex)}><FontAwesomeIcon icon={faTrash}/></button>
+                    <span className="float-right mr-3"><FontAwesomeIcon icon={faCaretDown}/></span>
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="0" className="collapsible-body pb-3">
+                    <Card.Body className="section-body">
+                        <ul className="sg-column-layout">
+                            {props.columnSizes.map((item, sizeIndex) => (
+                                props.column[props.columnIndex].active === sizeIndex ?
+                                    <li key={sizeIndex} className="sg-active">
+                                        {props.columnSizes[sizeIndex].items.map((item, index) => (
+                                            <span key={index} className={item.class}>
+                                                {item.size}
+                                            </span>
+                                        ))}
+                                    </li>
+                                :
+                                    <li key={sizeIndex} onClick={() => props.handleSizeActive(props.columnIndex, sizeIndex, item.id)}>
+                                        {props.columnSizes[sizeIndex].items.map((item, index) =>(
+                                            <span key={index} className={item.class}>
+                                                {item.size}
+                                            </span>
+                                        ))}
+                                    </li>
+                            ))}
+                        </ul>
+                    </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+        </Accordion>
+    )
 }
 
 export default Columns;
