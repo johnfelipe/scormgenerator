37d0d956beb12eff4a27e03e816072f2bc8cb1fb
zeejay09 <zeejaybelamide@gmail.com>
2020-05-21 16:19:59 +0800

    adds onMouseOver and onMouseLeave on textarea of the content area's editor interface

--- src/components/Slide/Editor.js
+++ src/components/Slide/Editor.js
@@ -3,12 +3,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 class Editor extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isShown: false,
+        };
+        
+        this.setIsShown = this.setIsShown.bind(this);
+    }
+
+    setIsShown = (value) => {
+        this.setState({
+            isShown: value,
+        })
+    }
+
     render() {
         return (
             <div className="sg-scroll-box sg-workspace-controls ps">
                 <div className="sg-controls">
                     <div className="sg-control sg-inspector-actions">
-                        <div class="sg-workspace-actions">
+                        <div className="sg-workspace-actions">
                             <button type="button" className="sg-workspace-action-item btn btn-link">
                                 <FontAwesomeIcon icon={faTrashAlt}/>
                                 <span>Delete</span>
@@ -22,8 +38,16 @@ class Editor extends Component {
                         <div className="sg-control-input">
                             <div className="sg-expandable-rich-text">
                                 <div className="sg-workspace-expander">
-                                    <div tabindex="-1" className="cs-workspace-expander-toggle ">
-
+                                    <div tabIndex="-1" className="sg-workspace-expander-toggle ">
+                                        { 
+                                            this.state.isShown ? 
+                                            <button className="textarea-hover-btn" onMouseLeave={() => this.setIsShown(false)}>
+                                                <span>Click to Edit</span>
+                                            </button>
+                                            :
+                                            <span></span>
+                                        }
+                                        <textarea onMouseOver={() => this.setIsShown(true)} disabled value="&lt;span&gt;This content will show up directly in its container.&lt;/span&gt;"/>
                                     </div>
                                 </div>
                             </div>
