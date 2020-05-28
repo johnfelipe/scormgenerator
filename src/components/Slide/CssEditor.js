bbfd3b25e46283a8586c1c13bb7d41733184003b
zeejay09 <zeejaybelamide@gmail.com>
2020-05-28 18:26:13 +0800

    continues to create the css editor

--- src/components/Slide/CssEditor.js
+++ src/components/Slide/CssEditor.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes, faCode } from '@fortawesome/free-solid-svg-icons';
-import {Controlled as CodeMirror} from 'react-codemirror2'
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import {Controlled as CodeMirror} from 'react-codemirror2';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
 
 class CssEditor extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            value: '<h1>I ♥ react-codemirror2</h1>',
+            value: 'this is still not editable',
         };
     }
 
     render() {
         return (
-            <div className={this.props.showCssEditor ? "sg-workspace-expander-content sg-workspace-expander-content-vertical sg-workspace-expander-content-expandable-text-editor sg-active" : "sg-workspace-expander-content sg-workspace-expander-content-vertical sg-workspace-expander-content-expandable-text-editor"}>
+            <div className={this.props.showCssEditor ? "sg-workspace-expander-content sg-workspace-expander-content-vertical sg-code-editor-wrapper sg-active" : "sg-workspace-expander-content sg-workspace-expander-content-vertical sg-code-editor-wrapper"}>
                 <div className="sg-workspace-expander-head">
-                    <div className="sg-workspace-expander-head-actions">
-                        <button type="button" className="sg-active">
-                            <FontAwesomeIcon icon={faCode}/>
-                        </button>
-                    </div>
                     <div className="sg-workspace-expander-head-label">
                         <span>Element CSS</span>
                     </div>
@@ -31,31 +28,30 @@ class CssEditor extends Component {
                     </div>
                 </div>
                 <div className="sg-workspace-expander-body">
-                    <CodeMirror
-                        value={this.state.value}
+                    <div className="sg-inline-code-editor h-100">
+                        <CodeMirror
+                            className="h-100"
+                            value={this.state.value}
 
-                        options={{
-                            mode: 'xml',
-                            theme: 'material',
-                            lineNumbers: true
-                        }}
+                            options={{
+                                mode: 'css',
+                                theme: 'material',
+                                lineNumbers: true
+                            }}
 
-                        onBeforeChange={(editor, data, value) => {
-                            this.setState({
-                                sampleValue: value,
-                            });
-                        }}
+                            onBeforeChange={(editor, data, value) => {
+                                this.setState({
+                                    sampleValue: value,
+                                });
+                            }}
 
-                        onChange={(editor, data, value) => {
-                            console.log(editor);
-                            console.log(data);
-                            console.log(value);
-                        }}
-                    />
-                    {/* <div className="sg-inline-code-editor h-100">
-                        
-                        
-                    </div> */}
+                            onChange={(editor, data, value) => {
+                                console.log(editor);
+                                console.log(data);
+                                console.log(value);
+                            }}
+                        />
+                    </div>
                 </div>
             </div>
         )
