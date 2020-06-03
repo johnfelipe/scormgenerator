38569436698916ff9c24d26fd4951ddf247a5a14
zeejay09 <zeejaybelamide@gmail.com>
2020-06-03 13:08:03 +0800

    adds provided.placeholder to avoid react dnd warnings

--- src/components/Handlers/SlideHandler.js
+++ src/components/Handlers/SlideHandler.js
@@ -1042,6 +1042,7 @@ class SlideHandler extends Component {
                                                                                         )}
                                                                                     </Draggable>
                                                                                 ))}
+                                                                                {provided.placeholder}
                                                                             </div>
                                                                         )}
                                                                     </Droppable>
