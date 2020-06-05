257ba2eb67f4c3c91575c9ff73b0877304f58315
zeejay09 <zeejaybelamide@gmail.com>
2020-06-05 13:21:41 +0800

    refactors MediaUploader

--- src/components/Gallery/MediaUploader.js
+++ src/components/Gallery/MediaUploader.js
@@ -3,62 +3,62 @@ import React, { Component } from 'react';
 // https://codepen.io/hartzis/pen/VvNGZP
 class MediaUploader extends Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        file: '',
-        imagePreviewUrl: ''
-      };
-      this._handleImageChange = this._handleImageChange.bind(this);
-      this._handleSubmit = this._handleSubmit.bind(this);
-    }
-  
-    _handleSubmit(e) {
-      e.preventDefault();
-      // TODO: do something with -> this.state.file
-    }
-  
-    _handleImageChange(e) {
-      e.preventDefault();
-      let files = e.target.files;
-
-      Object.keys(files).map((fileIndex) => {
-
-        let reader = new FileReader();
-
-        const fileObject = {
-          name: files[fileIndex].name,
-          size: files[fileIndex].size,
-          type: files[fileIndex].type,
-          lastModified: files[fileIndex].lastModified,
-          lastModifiedDate: files[fileIndex].lastModifiedDate,
+        super(props);
+        this.state = {
+            file: '',
+            imagePreviewUrl: ''
         };
-
-        this.props.setMediaFiles(fileObject, fileIndex);
-
-        reader.readAsDataURL(files[fileIndex])
-        reader.onloadend = () => {
-
-          const urlObject = {
-            name: files[fileIndex].name,
-            dataUrl: reader.result
-          };
-
-          this.props.setMediaUrls(urlObject);
-        }
-      });
+        this._handleImageChange = this._handleImageChange.bind(this);
+        this._handleSubmit = this._handleSubmit.bind(this);
     }
-  
+
+    _handleSubmit(e) {
+        e.preventDefault();
+        // TODO: do something with -> this.state.file
+    }
+
+    _handleImageChange(e) {
+        e.preventDefault();
+        let files = e.target.files;
+
+        Object.keys(files).map((fileIndex) => {
+
+            let reader = new FileReader();
+
+            const fileObject = {
+                name: files[fileIndex].name,
+                size: files[fileIndex].size,
+                type: files[fileIndex].type,
+                lastModified: files[fileIndex].lastModified,
+                lastModifiedDate: files[fileIndex].lastModifiedDate,
+            };
+
+            this.props.setMediaFiles(fileObject, fileIndex);
+
+            reader.readAsDataURL(files[fileIndex])
+            reader.onloadend = () => {
+
+                const urlObject = {
+                    name: files[fileIndex].name,
+                    dataUrl: reader.result
+                };
+
+                this.props.setMediaUrls(urlObject);
+            }
+        });
+    }
+
     render() {
 
-      return (
-        <div>
-          {/* <form onSubmit={this._handleSubmit}> */}
-            <input type="file" onChange={this._handleImageChange} multiple/>
-          {/* </form> */}
-        </div>
-      )
+        return (
+            <div>
+                {/* <form onSubmit={this._handleSubmit}> */}
+                <input type="file" onChange={this._handleImageChange} multiple />
+                {/* </form> */}
+            </div>
+        )
     }
-  
-  }
 
-  export default MediaUploader;
+}
+
+export default MediaUploader;
