c2b7b5a635f740f8d72fa1ecbaefb3e872045c95
zeejay09 <zeejaybelamide@gmail.com>
2020-06-05 16:39:06 +0800

    adds alert message to inform user of a successful upload

--- src/components/Gallery/MediaUploader.js
+++ src/components/Gallery/MediaUploader.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
+import Alert from 'react-bootstrap/Alert';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 // https://codepen.io/hartzis/pen/VvNGZP
 class MediaUploader extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            file: '',
-            imagePreviewUrl: ''
+            showSuccessMsg: false,
         };
-        this._handleImageChange = this._handleImageChange.bind(this);
-        this._handleSubmit = this._handleSubmit.bind(this);
+        this.handleImageChange = this.handleImageChange.bind(this);
     }
 
-    _handleSubmit(e) {
-        e.preventDefault();
-        // TODO: do something with -> this.state.file
+    componentDidUpdate = () => {
+        console.log("files");
+        console.log(this.state.files);
+        console.log("urls");
+        console.log(this.state.urls);
     }
 
-    _handleImageChange(e) {
+    handleImageChange(e) {
         e.preventDefault();
         let files = e.target.files;
 
+        this.setState({
+            showSuccessMsg: true,
+        })
+
+        // eslint-disable-next-line
         Object.keys(files).map((fileIndex) => {
 
             let reader = new FileReader();
@@ -51,10 +59,41 @@ class MediaUploader extends Component {
     render() {
 
         return (
-            <div>
-                {/* <form onSubmit={this._handleSubmit}> */}
-                <input type="file" onChange={this._handleImageChange} multiple />
-                {/* </form> */}
+            <div className="row mt-5">
+                <div className="col-md-4"></div>
+                <div className="text-center col-md-4">
+                    <div className="input-group">
+                        <div className="custom-file">
+                            <input
+                                type="file"
+                                className="custom-file-input"
+                                id="inputGroupFile01"
+                                aria-describedby="inputGroupFileAddon01"
+                                onChange={this.handleImageChange}
+                                multiple
+                            />
+                            <label className="custom-file-label pr-6" htmlFor="inputGroupFile01">
+                                {
+                                    this.state.showSuccessMsg ?
+                                        <span className="text-success">Files are successfully uploaded!</span>
+                                    :
+                                        <span>Choose file/s</span>
+                                }
+                            </label>
+                        </div>
+                    </div>
+                    {
+                        this.state.showSuccessMsg ?
+                            <div id="success-message">
+                                <Alert variant='success'>
+                                    <span><FontAwesomeIcon icon={faCheck}/> Successfully uploaded!</span>
+                                </Alert>
+                            </div>
+                        :
+                            null
+                    }
+                </div>
+                <div className="col-md-4"></div>
             </div>
         )
     }
