f1c90cba3ab4ebdce8848972f933db93e1ff517a
zeejay09 <zeejaybelamide@gmail.com>
2020-07-06 18:32:37 +0800

    reverts the other sizes and starts with the default column size

--- src/components/Handlers/SlideHandler.js
+++ src/components/Handlers/SlideHandler.js
@@ -153,45 +153,45 @@ class SlideHandler extends Component {
         columnSizesObj[columnIndex].grid = grid;
 
         if (grid === 1 || grid === 2 || grid === 3) {
-            // columnSizesObj[columnIndex].content['subColumnOne'] = [];
-            // columnSizesObj[columnIndex].content['subColumnTwo'] = [];
-            columnSizesObj[columnIndex].content = {
-                subColumnOne: [],
-                subColumnTwo: [],
-            };
+            columnSizesObj[columnIndex].content['subColumnOne'] = [];
+            columnSizesObj[columnIndex].content['subColumnTwo'] = [];
+            // columnSizesObj[columnIndex].content = {
+            //     subColumnOne: [],
+            //     subColumnTwo: [],
+            // };
         } else if (grid === 4) {
-            // columnSizesObj[columnIndex].content['subColumnOne'] = [];
-            // columnSizesObj[columnIndex].content['subColumnTwo'] = [];
-            // columnSizesObj[columnIndex].content['subColumnThree'] = [];
-            columnSizesObj[columnIndex].content = {
-                subColumnOne: [],
-                subColumnTwo: [],
-                subColumnThree: [],
-            };
+            columnSizesObj[columnIndex].content['subColumnOne'] = [];
+            columnSizesObj[columnIndex].content['subColumnTwo'] = [];
+            columnSizesObj[columnIndex].content['subColumnThree'] = [];
+            // columnSizesObj[columnIndex].content = {
+            //     subColumnOne: [],
+            //     subColumnTwo: [],
+            //     subColumnThree: [],
+            // };
         } else if (grid === 5) {
-            // columnSizesObj[columnIndex].content['subColumnOne'] = [];
-            // columnSizesObj[columnIndex].content['subColumnTwo'] = [];
-            // columnSizesObj[columnIndex].content['subColumnThree'] = [];
-            // columnSizesObj[columnIndex].content['subColumnFour'] = [];
-            columnSizesObj[columnIndex].content = {
-                subColumnOne: [],
-                subColumnTwo: [],
-                subColumnThree: [],
-                subColumnFour: [],
-            };
+            columnSizesObj[columnIndex].content['subColumnOne'] = [];
+            columnSizesObj[columnIndex].content['subColumnTwo'] = [];
+            columnSizesObj[columnIndex].content['subColumnThree'] = [];
+            columnSizesObj[columnIndex].content['subColumnFour'] = [];
+            // columnSizesObj[columnIndex].content = {
+            //     subColumnOne: [],
+            //     subColumnTwo: [],
+            //     subColumnThree: [],
+            //     subColumnFour: [],
+            // };
         } else if (grid === 6) {
-            // columnSizesObj[columnIndex].content['subColumnOne'] = [];
-            // columnSizesObj[columnIndex].content['subColumnTwo'] = [];
-            // columnSizesObj[columnIndex].content['subColumnThree'] = [];
-            // columnSizesObj[columnIndex].content['subColumnFour'] = [];
-            // columnSizesObj[columnIndex].content['subColumnFive'] = [];
-            columnSizesObj[columnIndex].content = {
-                subColumnOne: [],
-                subColumnTwo: [],
-                subColumnThree: [],
-                subColumnFour: [],
-                subColumnFive: [],
-            };
+            columnSizesObj[columnIndex].content['subColumnOne'] = [];
+            columnSizesObj[columnIndex].content['subColumnTwo'] = [];
+            columnSizesObj[columnIndex].content['subColumnThree'] = [];
+            columnSizesObj[columnIndex].content['subColumnFour'] = [];
+            columnSizesObj[columnIndex].content['subColumnFive'] = [];
+            // columnSizesObj[columnIndex].content = {
+            //     subColumnOne: [],
+            //     subColumnTwo: [],
+            //     subColumnThree: [],
+            //     subColumnFour: [],
+            //     subColumnFive: [],
+            // };
         }
 
         this.setState({
@@ -269,21 +269,21 @@ class SlideHandler extends Component {
                     
                     if (currentFeatures[source.index]['type'] === 'content-area') {
                         let currentContent = { type: currentFeatures[source.index]['type'], output: '<span>This content will show up directly in its container.</span>', class: '', id: '' };
-                        currentColumns[key].content['subColumnOne'].push(currentContent);
+                        currentColumns[key].content[0]['subColumnOne'].push(currentContent);
                         this.setState({
                             column: currentColumns,
                             activeFeature: currentFeatures[source.index]['type'],
                             activeColumnId: destination.index,
-                            activeContentIndex: (currentColumns[key].content['subColumnOne'].length - 1),
+                            activeContentIndex: (currentColumns[key].content[0]['subColumnOne'].length - 1),
                         });
                     } else if (currentFeatures[source.index]['type'] === 'audio') {
                         let currentContent = { type: currentFeatures[source.index]['type'], output: '', class: '', id: ''  };
-                        currentColumns[key].content['subColumnOne'].push(currentContent);
+                        currentColumns[key].content[0]['subColumnOne'].push(currentContent);
                         this.setState({
                             column: currentColumns,
                             activeFeature: currentFeatures[source.index]['type'],
                             activeColumnId: destination.index,
-                            activeContentIndex: (currentColumns[key].content['subColumnOne'].length - 1),
+                            activeContentIndex: (currentColumns[key].content[0]['subColumnOne'].length - 1),
                         });
                     }
                     
