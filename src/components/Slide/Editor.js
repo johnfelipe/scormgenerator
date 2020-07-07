d5087b1da57ea92dc6a856f5c06620e0673c2ec9
zeejay09 <zeejaybelamide@gmail.com>
2020-07-07 14:10:08 +0800

    creates a dedicated feature editor interface component for Content Area so that the editor component will not be cluttered with code

--- src/components/Slide/Editor.js
+++ src/components/Slide/Editor.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import ContentArea from './Features/ContentArea';
 
 class Editor extends Component {
 
@@ -76,130 +77,6 @@ class Editor extends Component {
     }
 
     render() {
-        const contentAreaInterface = (
-            <div className="sg-controls">
-                <div className="sg-control sg-inspector-actions">
-                    <div className="sg-workspace-actions">
-                        <button type="button" className="sg-workspace-action-item btn btn-link" onClick={() => this.deleteFeature(this.props.contentIndex)}>
-                            <FontAwesomeIcon icon={faTrashAlt}/>
-                            <span>Delete</span>
-                        </button>
-                    </div>
-                </div>
-                <div className="sg-control sg-control-text-editor">
-                    <div className="sg-control-header">
-                        <label>Content</label>
-                    </div>
-                    <div className="sg-control-input">
-                        <div className="sg-expandable-rich-text">
-                            <div className="sg-workspace-expander">
-                                <div tabIndex="-1" className="sg-workspace-expander-toggle ">
-                                    { 
-                                        this.state.isShownTextArea ? 
-                                        <button type="button" className="textarea-hover-btn btn btn-light" onMouseLeave={() => this.setIsShown(false)} onClick={() => this.props.setShowEditor(true, this.props.contentIndex)}>
-                                            <span>Click to Edit</span>
-                                        </button>
-                                        :
-                                        <span></span>
-                                    }
-                                    <textarea 
-                                        onMouseOver={() => this.setIsShown(true)} 
-                                        disabled 
-                                        value={ 
-                                            typeof this.props.currentColumn != "undefined" ? 
-                                                'content' in this.props.currentColumn && this.props.currentColumn.content[this.props.currentColumnContentIndex].length > 0 ? 
-                                                    this.props.currentColumnContentIndex in this.props.currentColumn.content && this.props.currentColumn.content[this.props.currentColumnContentIndex].length > 0 ?
-                                                        this.props.currentColumn.content[this.props.currentColumnContentIndex][this.props.contentIndex].output 
-                                                    :
-                                                        ''
-                                                : 
-                                                    ''
-                                            : 
-                                                ''
-                                        }
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="sg-control sg-control-group">
-                    <div className="sg-control-header">
-                        <label>Customize</label>
-                    </div>
-                    <div className="sg-control-input sg-control-input">
-                        <ul className="sg-control-input-list">
-                            <li className="sg-control-input-list-item sg-control-input-list-item-text">
-                                <div className="sg-control-input-list-label">
-                                    <span>ID</span>
-                                </div>
-                                <div className="sg-control-input-list-input">
-                                    <input
-                                        type="text"
-                                        placeholder=""
-                                        onChange={(event) => this.props.setFeatureId(event, this.props.contentIndex)}
-                                        value={ 
-                                            typeof this.props.currentColumn != "undefined" ? 
-                                                'content' in this.props.currentColumn && this.props.currentColumn.content[this.props.currentColumnContentIndex].length > 0 ? 
-                                                    this.props.currentColumnContentIndex in this.props.currentColumn.content && this.props.currentColumn.content[this.props.currentColumnContentIndex].length > 0 ?
-                                                        this.props.currentColumn.content[this.props.currentColumnContentIndex][this.props.contentIndex].id 
-                                                    :
-                                                        ''
-                                                : 
-                                                    ''
-                                            : 
-                                                ''
-                                        }
-                                    />
-                                </div>
-                            </li>
-                            <li className="sg-control-input-list-item sg-control-input-list-item-text">
-                                <div className="sg-control-input-list-label">
-                                    <span>Class</span>
-                                </div>
-                                <div className="sg-control-input-list-input">
-                                    <input
-                                        type="text"
-                                        placeholder=""
-                                        onChange={(event) => this.props.setFeatureClass(event, this.props.contentIndex)}
-                                        value={ 
-                                            typeof this.props.currentColumn != "undefined" ? 
-                                                'content' in this.props.currentColumn && this.props.currentColumn.content[this.props.currentColumnContentIndex].length > 0 ? 
-                                                    this.props.currentColumnContentIndex in this.props.currentColumn.content && this.props.currentColumn.content[this.props.currentColumnContentIndex].length > 0 ?
-                                                        this.props.currentColumn.content[this.props.currentColumnContentIndex][this.props.contentIndex].class 
-                                                    :
-                                                        ''
-                                                : 
-                                                    ''
-                                            : 
-                                                ''
-                                        }
-                                    />
-                                </div>
-                            </li>
-                            <li className="sg-control-input-list-item sg-control-input-list-item-text">
-                                <div className="sg-control-input-list-label">
-                                    <span>Element CSS</span>
-                                </div>
-                                <div className="sg-control-input-list-input">
-                                    <div className="sg-expandable-code-editor">
-                                        <div className="sg-workspace-expander">
-                                            <div tabIndex="-1" className="sg-workspace-expander-toggle ">
-                                                <button type="button" className="input-hover-btn btn btn-light border border-secondary p-1" onClick={() => this.props.setShowCssEditor(true, this.props.contentIndex)}>
-                                                    <span>Add CSS</span>
-                                                </button>
-                                                <input type="text" value="" disabled className="rounded"/>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        );
-
         const audioInterface = (
             <div className="sg-controls">
                 <div className="sg-control sg-inspector-actions">
@@ -338,7 +215,17 @@ class Editor extends Component {
                         audioInterface
                     :
                         this.props.feature === "content-area" ?
-                            contentAreaInterface
+                            <ContentArea
+                                deleteFeature={this.props.deleteFeature}
+                                contentIndex={this.props.contentIndex}
+                                setIsShown={this.props.setIsShown}
+                                setShowEditor={this.props.setShowEditor}
+                                currentColumn={this.props.currentColumn}
+                                currentColumnContentIndex={this.props.currentColumnContentIndex}
+                                setFeatureId={this.props.setFeatureId}
+                                setFeatureClass={this.props.setFeatureClass}
+                                setShowCssEditor={this.props.setShowCssEditor}
+                            />
                         :
                             message
                 }
---
+++ src/components/Slide/Features/ContentArea.js
@@ -0,0 +1,134 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
+function ContentArea(props) {
+
+    const [isShownTextArea, setIsShownTextArea] = useState(false);
+
+    return (
+        <div className="sg-controls">
+            <div className="sg-control sg-inspector-actions">
+                <div className="sg-workspace-actions">
+                    <button type="button" className="sg-workspace-action-item btn btn-link" onClick={() => props.deleteFeature(props.contentIndex)}>
+                        <FontAwesomeIcon icon={faTrashAlt}/>
+                        <span>Delete</span>
+                    </button>
+                </div>
+            </div>
+            <div className="sg-control sg-control-text-editor">
+                <div className="sg-control-header">
+                    <label>Content</label>
+                </div>
+                <div className="sg-control-input">
+                    <div className="sg-expandable-rich-text">
+                        <div className="sg-workspace-expander">
+                            <div tabIndex="-1" className="sg-workspace-expander-toggle ">
+                                { 
+                                    isShownTextArea ? 
+                                        <button type="button" className="textarea-hover-btn btn btn-light" onMouseLeave={() => setIsShownTextArea(false)} onClick={() => props.setShowEditor(true, props.contentIndex)}>
+                                            <span>Click to Edit</span>
+                                        </button>
+                                    :
+                                        <span></span>
+                                }
+                                <textarea 
+                                    onMouseOver={() => setIsShownTextArea(true)} 
+                                    disabled 
+                                    value={ 
+                                        typeof props.currentColumn != "undefined" ? 
+                                            'content' in props.currentColumn && props.currentColumn.content[props.currentColumnContentIndex].length > 0 ? 
+                                                props.currentColumnContentIndex in props.currentColumn.content && props.currentColumn.content[props.currentColumnContentIndex].length > 0 ?
+                                                    props.currentColumn.content[props.currentColumnContentIndex][props.contentIndex].output 
+                                                :
+                                                    ''
+                                            : 
+                                                ''
+                                        : 
+                                            ''
+                                    }
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="sg-control sg-control-group">
+                <div className="sg-control-header">
+                    <label>Customize</label>
+                </div>
+                <div className="sg-control-input sg-control-input">
+                    <ul className="sg-control-input-list">
+                        <li className="sg-control-input-list-item sg-control-input-list-item-text">
+                            <div className="sg-control-input-list-label">
+                                <span>ID</span>
+                            </div>
+                            <div className="sg-control-input-list-input">
+                                <input
+                                    type="text"
+                                    placeholder=""
+                                    onChange={(event) => props.setFeatureId(event, props.contentIndex)}
+                                    value={ 
+                                        typeof props.currentColumn != "undefined" ? 
+                                            'content' in props.currentColumn && props.currentColumn.content[props.currentColumnContentIndex].length > 0 ? 
+                                                props.currentColumnContentIndex in props.currentColumn.content && props.currentColumn.content[props.currentColumnContentIndex].length > 0 ?
+                                                    props.currentColumn.content[props.currentColumnContentIndex][props.contentIndex].id 
+                                                :
+                                                    ''
+                                            : 
+                                                ''
+                                        : 
+                                            ''
+                                    }
+                                />
+                            </div>
+                        </li>
+                        <li className="sg-control-input-list-item sg-control-input-list-item-text">
+                            <div className="sg-control-input-list-label">
+                                <span>Class</span>
+                            </div>
+                            <div className="sg-control-input-list-input">
+                                <input
+                                    type="text"
+                                    placeholder=""
+                                    onChange={(event) => props.setFeatureClass(event, props.contentIndex)}
+                                    value={ 
+                                        typeof props.currentColumn != "undefined" ? 
+                                            'content' in props.currentColumn && props.currentColumn.content[props.currentColumnContentIndex].length > 0 ? 
+                                                props.currentColumnContentIndex in props.currentColumn.content && props.currentColumn.content[props.currentColumnContentIndex].length > 0 ?
+                                                    props.currentColumn.content[props.currentColumnContentIndex][props.contentIndex].class 
+                                                :
+                                                    ''
+                                            : 
+                                                ''
+                                        : 
+                                            ''
+                                    }
+                                />
+                            </div>
+                        </li>
+                        <li className="sg-control-input-list-item sg-control-input-list-item-text">
+                            <div className="sg-control-input-list-label">
+                                <span>Element CSS</span>
+                            </div>
+                            <div className="sg-control-input-list-input">
+                                <div className="sg-expandable-code-editor">
+                                    <div className="sg-workspace-expander">
+                                        <div tabIndex="-1" className="sg-workspace-expander-toggle ">
+                                            <button type="button" className="input-hover-btn btn btn-light border border-secondary p-1" onClick={() => props.setShowCssEditor(true, props.contentIndex)}>
+                                                <span>Add CSS</span>
+                                            </button>
+                                            <input type="text" value="" disabled className="rounded"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ContentArea;
