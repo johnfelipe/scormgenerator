34de20fc7da31ee04322b7e8924307ae251e8fcd
zeejay09 <zeejaybelamide@gmail.com>
2020-07-13 10:26:04 +0800

    wraps the home page layout in a div

--- src/components/Slide/Layouts/HomePageLayout.js
+++ src/components/Slide/Layouts/HomePageLayout.js
@@ -9,18 +9,20 @@ function HomePageLayout(props) {
     const backgroundImg = props.backgroundImg.url;
 
     return (
-        <div className="home-page-container h-100 w-100" style={{ backgroundImage: 'url("' + backgroundImg + '")', backgroundSize: 'cover' }}>
-            <div className="slide">
-                <div className="course-name">
-                    <h2 className="course-header">{title}</h2>
-                <div className="course-bar"></div>
-                <h3 className="course-sub-header">
-                    <span>{subtitle}</span>
-                    <br/>
-                    <span>{date}</span>
-                    <br/>
-                    <span>COURSE ID: {courseId}</span>
-                </h3>
+        <div id="home-page-layout">
+            <div className="home-page-container h-100 w-100" style={{ backgroundImage: 'url("' + backgroundImg + '")', backgroundSize: 'cover' }}>
+                <div className="slide">
+                    <div className="course-name">
+                        <h2 className="course-header">{title}</h2>
+                    <div className="course-bar"></div>
+                    <h3 className="course-sub-header">
+                        <span>{subtitle}</span>
+                        <br/>
+                        <span>{date}</span>
+                        <br/>
+                        <span>COURSE ID: {courseId}</span>
+                    </h3>
+                    </div>
                 </div>
             </div>
         </div>
