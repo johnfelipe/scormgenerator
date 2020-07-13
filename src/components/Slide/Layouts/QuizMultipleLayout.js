49ce9748f782aef73b98e95912dc2dcdad3f62d6
zeejay09 <zeejaybelamide@gmail.com>
2020-07-13 16:24:16 +0800

    modifies the presentation of the checkboxes

--- src/components/Slide/Layouts/QuizMultipleLayout.js
+++ src/components/Slide/Layouts/QuizMultipleLayout.js
@@ -5,17 +5,20 @@ function QuizMultipleLayout(props) {
     const quiz = props.quiz
 
     return (
-        <div id="quiz-multiple-layout">
+        <div id="quiz-multiple-layout" className="w-100 h-100 p-3">
             {
                 quiz.length > 0 ?
                     quiz.map((item, itemIndex) => (
                         <div key={"quiz-question-" + itemIndex} className="question-group">
                             <h3><span>{(itemIndex+1) + '. ' + item.question}</span></h3>
-                            <ul className="quiz-question-answers">
+                            <ul className="quiz-question-answers list-unstyled">
                                 {
                                     item.answers.map((answer, answerIndex) => (
                                         <li key={"quiz-question-answer" + answerIndex}>
-                                            <span>{answer.answer}</span>
+                                            <div class="custom-control custom-checkbox mb-3">
+                                                <input type="checkbox" class="custom-control-input" id={"answer-" + answerIndex} name={answer.answer} disabled={true}/>
+                                                <label class="custom-control-label" for={"answer-" + answerIndex}>{answer.answer}</label>
+                                            </div>
                                         </li>
                                     ))
                                 }
