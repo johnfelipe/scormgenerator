46a91a3d71199c6a8c2c85f8001735b327bd9f36
zeejay09 <zeejaybelamide@gmail.com>
2020-07-14 15:03:47 +0800

    modifies the quiz layout component to adhere with the design changes

--- src/components/Slide/Layouts/QuizLayout.js
+++ src/components/Slide/Layouts/QuizLayout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { objectHelpers } from '../../../helpers';
 
 function QuizMultipleLayout(props) {
     
@@ -9,20 +10,47 @@ function QuizMultipleLayout(props) {
             {
                 quiz.length > 0 ?
                     quiz.map((item, itemIndex) => (
-                        <div key={"quiz-question-" + itemIndex} className="question-group">
-                            <h3><span>{(itemIndex+1) + '. ' + item.question}</span></h3>
-                            <ul className="quiz-question-answers list-unstyled">
-                                {
-                                    item.answers.map((answer, answerIndex) => (
-                                        <li key={"quiz-question-answer" + answerIndex}>
-                                            <div class="custom-control custom-checkbox mb-3">
-                                                <input type="checkbox" class="custom-control-input" id={"answer-" + answerIndex} name={answer.answer} disabled={true}/>
-                                                <label class="custom-control-label" for={"answer-" + answerIndex}>{answer.answer}</label>
-                                            </div>
-                                        </li>
-                                    ))
-                                }
-                            </ul>
+                        <div key={"quiz-question-" + itemIndex} className="question-group row">
+                            <div className="col-md-12">
+                                <p className="font-20"><span>{(itemIndex+1) + '. ' + item.question}</span></p>
+                            </div>
+                            {
+                                objectHelpers.isEmpty(item.img) ?
+                                    <div className="col-md-12 ml-5">
+                                        <ul className="quiz-question-answers list-unstyled">
+                                            {
+                                                item.answers.map((answer, answerIndex) => (
+                                                    <li key={"quiz-question-answer" + answerIndex}>
+                                                        <div class="custom-control custom-checkbox mb-3">
+                                                            <input type="checkbox" class="custom-control-input" id={"answer-" + answerIndex} name={answer.answer} disabled={true}/>
+                                                            <label class="custom-control-label" for={"answer-" + answerIndex}>{answer.answer}</label>
+                                                        </div>
+                                                    </li>
+                                                ))
+                                            }
+                                        </ul>
+                                    </div>
+                                :
+                                    <>
+                                        <div class="col-md-4">
+                                            <img src={item.img.url} className="w-100 h-auto" alt="Relation to the question"/>
+                                        </div>
+                                        <div className="col-md-8">
+                                            <ul className="quiz-question-answers list-unstyled">
+                                                {
+                                                    item.answers.map((answer, answerIndex) => (
+                                                        <li key={"quiz-question-answer" + answerIndex}>
+                                                            <div class="custom-control custom-checkbox mb-3">
+                                                                <input type="checkbox" class="custom-control-input" id={"answer-" + answerIndex} name={answer.answer} disabled={true}/>
+                                                                <label class="custom-control-label" for={"answer-" + answerIndex}>{answer.answer}</label>
+                                                            </div>
+                                                        </li>
+                                                    ))
+                                                }
+                                            </ul>
+                                        </div>
+                                    </>
+                            }
                         </div>
                     ))
                 :
