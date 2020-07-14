cf7d91597387b2ea518029626330ffb0e914e4c5
zeejay09 <zeejaybelamide@gmail.com>
2020-07-14 15:03:31 +0800

    creates helper component

---
+++ src/helpers/object.js
@@ -0,0 +1,11 @@
+export const objectHelpers = {
+    isEmpty,
+};
+
+function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
