761e117a28e1ade882cebbefc303a71a8fbacf21
zeejay09 <zeejaybelamide@gmail.com>
2020-07-15 14:06:42 +0800

    creates a method that returns the content via the content of the output of a feature

--- src/components/Slide/Layouts/QuizLayout.js
+++ src/components/Slide/Layouts/QuizLayout.js
@@ -4,8 +4,106 @@ import ReactAudioPlayer from 'react-audio-player';
 
 function QuizMultipleLayout(props) {
     
-    const quiz = props.quiz
+    const quiz = props.quiz;
 
+    const content = (item) => {
+        if ((objectHelpers.isEmpty(item, 'audio') === true) && (objectHelpers.isEmpty(item, 'img') === true)) {
+            return (
+                <>
+                    <div class="col-md-4">
+                        <img src={item.img.url} className="w-100 h-auto" alt="Relation to the question"/>
+                    </div>
+                    <div className="col-md-8">
+                        <ul className="quiz-question-answers list-unstyled">
+                            {
+                                item.answers.map((answer, answerIndex) => (
+                                    <li key={"quiz-question-answer" + answerIndex}>
+                                        <div class="custom-control custom-checkbox mb-2">
+                                            <input type="checkbox" class="custom-control-input" id={"answer-" + answerIndex} name={answer.answer} disabled={true}/>
+                                            <label class="custom-control-label" htmlFor={"answer-" + answerIndex}>{answer.answer}</label>
+                                        </div>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                    <div className="col-md-12">
+                        <ReactAudioPlayer
+                            src={item.audio.url}
+                            controls
+                            title={item.audio.name}
+                        />
+                    </div>
+                </>
+            );
+        } else if (objectHelpers.isEmpty(item, 'img')) {
+            return (
+                <>
+                    <div class="col-md-4">
+                        <img src={item.img.url} className="w-100 h-auto" alt="Relation to the question"/>
+                    </div>
+                    <div className="col-md-8">
+                        <ul className="quiz-question-answers list-unstyled">
+                            {
+                                item.answers.map((answer, answerIndex) => (
+                                    <li key={"quiz-question-answer" + answerIndex}>
+                                        <div class="custom-control custom-checkbox mb-2">
+                                            <input type="checkbox" class="custom-control-input" id={"answer-" + answerIndex} name={answer.answer} disabled={true}/>
+                                            <label class="custom-control-label" htmlFor={"answer-" + answerIndex}>{answer.answer}</label>
+                                        </div>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                </>
+            );
+        } else if (objectHelpers.isEmpty(item, 'audio')) {
+            return (
+                <>
+                    <div className="col-md-12">
+                        <ul className="quiz-question-answers list-unstyled pl-5">
+                            {
+                                item.answers.map((answer, answerIndex) => (
+                                    <li key={"quiz-question-answer" + answerIndex}>
+                                        <div class="custom-control custom-checkbox mb-2">
+                                            <input type="checkbox" class="custom-control-input" id={"answer-" + answerIndex} name={answer.answer} disabled={true}/>
+                                            <label class="custom-control-label" htmlFor={"answer-" + answerIndex}>{answer.answer}</label>
+                                        </div>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                    <div className="col-md-12">
+                        <ReactAudioPlayer
+                            src={item.audio.url}
+                            controls
+                            title={item.audio.name}
+                        />
+                    </div>
+                </>
+            );
+        } else {
+            return (
+                <div className="col-md-12 ml-5">
+                    <ul className="quiz-question-answers list-unstyled pl-5">
+                        {
+                            item.answers.map((answer, answerIndex) => (
+                                <li key={"quiz-question-answer" + answerIndex}>
+                                    <div class="custom-control custom-checkbox mb-2">
+                                        <input type="checkbox" class="custom-control-input" id={"answer-" + answerIndex} name={answer.answer} disabled={true}/>
+                                        <label class="custom-control-label" for={"answer-" + answerIndex}>{answer.answer}</label>
+                                    </div>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            );
+        }
+    }
+    
     return (
         <div id="quiz-multiple-layout" className="w-100 h-100 p-3">
             {
@@ -16,49 +114,8 @@ function QuizMultipleLayout(props) {
                                 <p className="font-20"><span>{(itemIndex+1) + '. ' + item.question}</span></p>
                             </div>
                             {
-                                objectHelpers.isEmpty(item.img) ?
-                                    <div className="col-md-12 ml-5">
-                                        <ul className="quiz-question-answers list-unstyled">
-                                            {
-                                                item.answers.map((answer, answerIndex) => (
-                                                    <li key={"quiz-question-answer" + answerIndex}>
-                                                        <div class="custom-control custom-checkbox mb-2">
-                                                            <input type="checkbox" class="custom-control-input" id={"answer-" + answerIndex} name={answer.answer} disabled={true}/>
-                                                            <label class="custom-control-label" for={"answer-" + answerIndex}>{answer.answer}</label>
-                                                        </div>
-                                                    </li>
-                                                ))
-                                            }
-                                        </ul>
-                                    </div>
-                                :
-                                    <>
-                                        <div class="col-md-4">
-                                            <img src={item.img.url} className="w-100 h-auto" alt="Relation to the question"/>
-                                        </div>
-                                        <div className="col-md-8">
-                                            {
-                                                objectHelpers.isEmpty(item.audio) === false &&
-                                                <ReactAudioPlayer
-                                                    src={item.audio.url}
-                                                    controls
-                                                    title={item.audio.name}
-                                                />
-                                            }
-                                            <ul className="quiz-question-answers list-unstyled">
-                                                {
-                                                    item.answers.map((answer, answerIndex) => (
-                                                        <li key={"quiz-question-answer" + answerIndex}>
-                                                            <div class="custom-control custom-checkbox mb-2">
-                                                                <input type="checkbox" class="custom-control-input" id={"answer-" + answerIndex} name={answer.answer} disabled={true}/>
-                                                                <label class="custom-control-label" htmlFor={"answer-" + answerIndex}>{answer.answer}</label>
-                                                            </div>
-                                                        </li>
-                                                    ))
-                                                }
-                                            </ul>
-                                        </div>
-                                    </>
+                                
+                                    content(item)
                             }
                         </div>
                     ))
