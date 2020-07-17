4544de3480f734ed477b2a262705cea6cc8b5619
zeejay09 <zeejaybelamide@gmail.com>
2020-07-17 16:34:43 +0800

    changes from class style to react hooks

--- src/components/AlertModal/ChangeGridWarning.js
+++ src/components/AlertModal/ChangeGridWarning.js
@@ -1,52 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Modal } from 'react-bootstrap';
 
-class ChangeGrid extends Component {
+function ChangeGrid (props) {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            modalShow: false,
-        };
-        this.setModalShow = this.setModalShow.bind(this);
-    }
+    const modalShow = props.modalShow;
+    // const removeFeatures = props.removeFeatures;
 
-    setModalShow = (value) => {
-        this.setState({
-            modalShow: value,
-        });
-    }
+    const confirmationModal = (
+        <Modal
+            show={modalShow}
+            onHide={() => props.setModalShow(false)}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Warning
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <span>Features exists in this column grid. Do you want to remove all features and change grid size?</span>
+            </Modal.Body>
+            <Modal.Footer>
+                <button
+                    className="btn btn-primary"
+                    onClick={() => {
+                        props.setModalShow(false);
+                        props.setRemoveFeaures(true);
+                    }}
+                >
+                    Yes
+                </button>
+                <button
+                    className="btn btn-danger"
+                    onClick={() => {
+                        props.setModalShow(false);
+                        props.setRemoveFeaures(false);
+                    }}
+                >
+                    No
+                </button>
+            </Modal.Footer>
+        </Modal>
+    );
 
-    render() {
-        const confirmationModal = (
-            <Modal
-                show={this.state.modalShow}
-                onHide={() => this.setModalShow(false)}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        Warning
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <span>Features exists in this column grid. Do you want to remove all features and change grid size?</span>
-                </Modal.Body>
-                <Modal.Footer>
-                    <button className="btn btn-priamry" onClick={() => this.setModalShow(false)}>Yes</button>
-                    <button className="btn btn-danger" onClick={() => this.setModalShow(false)}>No</button>
-                </Modal.Footer>
-            </Modal>
-        );
-
-        return (
-            <>
-                {confirmationModal}
-            </>
-        )
-    }
+    return (
+        <>
+            {confirmationModal}
+        </>
+    )
 }
 
 export default ChangeGrid;
