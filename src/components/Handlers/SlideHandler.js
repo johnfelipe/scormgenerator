ddef982f2c76560b72a0b3b27ed8130e9f1b1355
zeejay09 <zeejaybelamide@gmail.com>
2020-07-20 14:57:24 +0800

    passes necessary props to save the label value to the state

--- src/components/Handlers/SlideHandler.js
+++ src/components/Handlers/SlideHandler.js
@@ -1389,6 +1389,10 @@ class SlideHandler extends Component {
                                                                                                                             quizClass={contentFirst.class}
                                                                                                                             quizId={contentFirst.id}
                                                                                                                             quizStyles={contentFirst.styles}
+                                                                                                                            currentColumn={this.state.column[this.state.activeColumnId]}
+                                                                                                                            contentIndex={this.state.activeContentIndex}
+                                                                                                                            currentColumnContentIndex={this.state.currentColumnContentIndex}
+                                                                                                                            setColumn={this.setColumn}
                                                                                                                         />
                                                                                                                     :
                                                                                                                         ReactHtmlParser(contentFirst.output)
