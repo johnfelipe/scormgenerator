799a8c19ea0d63322c66e074228a11c36c060fef
zeejay09 <zeejaybelamide@gmail.com>
2020-07-22 18:59:04 +0800

    adds default background and classes

--- src/components/Slide/Layouts/CourseObjLayout.js
+++ src/components/Slide/Layouts/CourseObjLayout.js
@@ -9,10 +9,10 @@ function CourseObjLayout(props) {
             <div className="course-objective-container h-100 w-100 border border-light">
                 <div className="row">
                     <div className="col-md-8">
-                        <h2 className="course-header">yes</h2>
+                        <h2 className="course-header">Sample</h2>
                     </div>
                     <div className="col-md-4">
-                        <div className="course-intro-wrapper" style={{ background: styles.courseIntroColor}}>
+                        <div className="course-intro-wrapper w-100 h-100 p-4" style={{ background: styles.courseIntroColor}}>
                             <div className="mt-5">
                                 <div type="button" className="mt-3">Course Navigation</div>
                                 <div type="button" className="mt-3">Course Information</div>
