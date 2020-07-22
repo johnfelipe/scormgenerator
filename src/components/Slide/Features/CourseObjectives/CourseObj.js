2ac463d1d5729fed3fdff00017aa54d641bd37b1
zeejay09 <zeejaybelamide@gmail.com>
2020-07-22 17:08:34 +0800

    adds new controls for content management

--- src/components/Slide/Features/CourseObjectives/CourseObj.js
+++ src/components/Slide/Features/CourseObjectives/CourseObj.js
@@ -24,12 +24,12 @@ function CourseObj(props) {
                     <label>Content</label>
                 </div>
                 <div className="sg-control-input">
-                    <div className="sg-expandable-rich-text">
+                    {/* <div className="sg-expandable-rich-text">
                         <div className="sg-workspace-expander">
                             <div tabIndex="-1" className="sg-workspace-expander-toggle ">
                                 { 
                                     isShownTextArea ? 
-                                        <button type="button" className="textarea-hover-btn btn btn-light" onMouseLeave={() => setIsShownTextArea(false)} onClick={() => props.setShowEditor(true, props.contentIndex)}>
+                                        <button type="button" className="textarea-hover-btn btn btn-light" onMouseLeave={() => setIsShownTextArea(false)} onClick={() => {props.setShowTextEditor(true, props.contentIndex); console.log('text')}}>
                                             <span>Click to Edit</span>
                                         </button>
                                     :
@@ -53,14 +53,81 @@ function CourseObj(props) {
                                 />
                             </div>
                         </div>
-                    </div>
+                    </div> */}
+                    <ul className="sg-control-input-list">
+                        <li className="sg-control-input-list-item sg-control-input-list-item-text">
+                            <div className="sg-control-input-list-label">
+                                <span>Course Information</span>
+                            </div>
+                            <div className="sg-control-input-list-input">
+                                <div className="sg-expandable-code-editor">
+                                    <div className="sg-workspace-expander">
+                                        <div tabIndex="-1" className="sg-workspace-expander-toggle ">
+                                            <button type="button" className="input-hover-btn btn btn-light border border-secondary p-1" onClick={() => props.setShowTextEditor(true, props.contentIndex, 'courseInfo')}>
+                                                <span>Edit</span>
+                                            </button>
+                                            <input type="text" value="" disabled className="rounded"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="sg-control-input-list-item sg-control-input-list-item-text">
+                            <div className="sg-control-input-list-label">
+                                <span>ID</span>
+                            </div>
+                            <div className="sg-control-input-list-input">
+                                <input
+                                    type="text"
+                                    placeholder=""
+                                    onChange={(event) => props.setFeatureId(event, props.contentIndex)}
+                                    value={ 
+                                        typeof props.currentColumn != "undefined" ? 
+                                            'content' in props.currentColumn && props.currentColumn.content[props.currentColumnContentIndex].length > 0 ? 
+                                                props.currentColumnContentIndex in props.currentColumn.content && props.currentColumn.content[props.currentColumnContentIndex].length > 0 ?
+                                                    props.currentColumn.content[props.currentColumnContentIndex][props.contentIndex].id 
+                                                :
+                                                    ''
+                                            : 
+                                                ''
+                                        : 
+                                            ''
+                                    }
+                                />
+                            </div>
+                        </li>
+                        <li className="sg-control-input-list-item sg-control-input-list-item-text">
+                            <div className="sg-control-input-list-label">
+                                <span>Class</span>
+                            </div>
+                            <div className="sg-control-input-list-input">
+                                <input
+                                    type="text"
+                                    placeholder=""
+                                    onChange={(event) => props.setFeatureClass(event, props.contentIndex)}
+                                    value={ 
+                                        typeof props.currentColumn != "undefined" ? 
+                                            'content' in props.currentColumn && props.currentColumn.content[props.currentColumnContentIndex].length > 0 ? 
+                                                props.currentColumnContentIndex in props.currentColumn.content && props.currentColumn.content[props.currentColumnContentIndex].length > 0 ?
+                                                    props.currentColumn.content[props.currentColumnContentIndex][props.contentIndex].class 
+                                                :
+                                                    ''
+                                            : 
+                                                ''
+                                        : 
+                                            ''
+                                    }
+                                />
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
             <div className="sg-control sg-control-group">
                 <div className="sg-control-header">
                     <label>Customize</label>
                 </div>
-                <div className="sg-control-input sg-control-input">
+                <div className="sg-control-input">
                     <ul className="sg-control-input-list">
                         <li className="sg-control-input-list-item sg-control-input-list-item-text">
                             <div className="sg-control-input-list-label">
