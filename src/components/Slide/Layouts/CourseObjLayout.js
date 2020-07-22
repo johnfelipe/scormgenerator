bda8d6d86d997848828a96f59a72d649ebed423a
zeejay09 <zeejaybelamide@gmail.com>
2020-07-22 19:36:13 +0800

    replaces the Video Player component with the HTML5 Video tag

--- src/components/Slide/Layouts/CourseObjLayout.js
+++ src/components/Slide/Layouts/CourseObjLayout.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { Player, ControlBar } from 'video-react';
 
 function CourseObjLayout(props) {
 
     const styles = props.styles;
+    const introVideo = props.content.introVideo;
 
     return (
         <div id="course-objective-layout">
             <div className="course-objective-container h-100 w-100 border border-light">
                 <div className="row">
                     <div className="col-md-8">
-                        <h2 className="course-header">Sample</h2>
+                        {/* <Player className="course-obj-itro-video h-100 w-100">
+                            <source src={introVideo} />
+                            
+                            <ControlBar autoHide={true}/>
+                        </Player> */}
+                        <video className="course-obj-itro-video h-100 w-100" controls>
+                            <source src={introVideo}/>
+                            {/* <source src="movie.ogg" type="video/ogg"/> */}
+                            Your browser does not support the video tag.
+                        </video>
                     </div>
                     <div className="col-md-4">
                         <div className="course-intro-wrapper w-100 h-100 p-4" style={{ background: styles.courseIntroColor}}>
