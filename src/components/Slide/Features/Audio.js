53ff6d714207ac5dc47b58614667765a4a8158b7
zeejay09 <zeejaybelamide@gmail.com>
2020-07-23 16:32:01 +0800

    adds a reset feature button on the audio feature component

--- src/components/Slide/Features/Audio.js
+++ src/components/Slide/Features/Audio.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { faTrashAlt, faUndo } from '@fortawesome/free-solid-svg-icons';
 
 function Audio(props) {
 
@@ -8,6 +8,10 @@ function Audio(props) {
         <div className="sg-controls">
             <div className="sg-control sg-inspector-actions">
                 <div className="sg-workspace-actions">
+                    <button type="button" className="sg-workspace-action-item btn btn-link border-right rounded-0" onClick={() => props.resetFeature(props.contentIndex, 'audio')}>
+                        <FontAwesomeIcon icon={faUndo}/>
+                        <span>Reset</span>
+                    </button>
                     <button type="button" className="sg-workspace-action-item btn btn-link" onClick={() => props.deleteFeature(props.contentIndex)}>
                         <FontAwesomeIcon icon={faTrashAlt}/>
                         <span>Delete</span>
