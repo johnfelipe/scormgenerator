5814d358c264472d38f30972e0deb0dff7e2e7df
zeejay09 <zeejaybelamide@gmail.com>
2020-07-23 17:07:13 +0800

    adds reset button in course objectives feature editor interface

--- src/components/Slide/Features/CourseObjectives/CourseObj.js
+++ src/components/Slide/Features/CourseObjectives/CourseObj.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { faTrashAlt, faUndo } from '@fortawesome/free-solid-svg-icons';
 
 function CourseObj(props) {
     
@@ -12,6 +12,10 @@ function CourseObj(props) {
         <div className="sg-controls">
             <div className="sg-control sg-inspector-actions">
                 <div className="sg-workspace-actions">
+                    <button type="button" className="sg-workspace-action-item btn btn-link border-right rounded-0" onClick={() => props.resetFeature(contentIndex, 'courseObjectives')}>
+                        <FontAwesomeIcon icon={faUndo}/>
+                        <span>Reset</span>
+                    </button>
                     <button type="button" className="sg-workspace-action-item btn btn-link" onClick={() => props.deleteFeature(contentIndex)}>
                         <FontAwesomeIcon icon={faTrashAlt}/>
                         <span>Delete</span>
