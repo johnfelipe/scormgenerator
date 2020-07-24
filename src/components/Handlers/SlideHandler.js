7f87f9706ff515eed95eaec290c42022cf04d93b
zeejay09 <zeejaybelamide@gmail.com>
2020-07-24 17:01:52 +0800

    makes the default output of the course objectives is array

--- src/components/Handlers/SlideHandler.js
+++ src/components/Handlers/SlideHandler.js
@@ -311,7 +311,7 @@ class SlideHandler extends Component {
                             activeContentIndex: (currentColumns[key].content.subColumnOne.length - 1),
                         });
                     } else if (currentFeatures[source.index]['type'] === 'courseObjectives') {
-                        let currentContent = { type: currentFeatures[source.index]['type'], output: { courseInfo: { name: 'Course Information', content: 'No information provided yet.'}, courseReq:  { name: 'Course Requirements', content: 'No requirements provided yet.'}, introVideo: 'https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4' }, class: '', id: '', styles: { courseIntroColor: '#0069d9' } };
+                        let currentContent = { type: currentFeatures[source.index]['type'], output: [ { name: 'Course Information', content: 'No information provided yet.'}, { name: 'Course Requirements', content: 'No requirements provided yet.'}, ], class: '', id: '', styles: { courseIntroColor: '#0069d9' }, introVideo: 'https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4' };
                         currentColumns[key].content.subColumnOne.push(currentContent);
                         this.setState({
                             column: currentColumns,
@@ -1143,7 +1143,7 @@ class SlideHandler extends Component {
         } else if (featureType === "homePage") {
             currentColumnObj.content[currentColumnContentIndex][contentIndex] = { type: 'homePage', output: { title: 'Title', subtitle: 'Subtitle', date: 'January 1970', courseId: '1234567890', backgroundImg: { name: '', url: '' } }, class: 'course-title-bottom-left', id: '', colorScheme: { titleBoxColor: '#0069d9' } };
         } else if (featureType === "courseObjectives") {
-            currentColumnObj.content[currentColumnContentIndex][contentIndex] = { type: 'courseObjectives', output: { courseInfo: { name: 'Course Information', content: 'No information provided yet.'}, courseReq:  { name: 'Course Requirements', content: 'No requirements provided yet.'}, introVideo: 'https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4' }, class: '', id: '', styles: { courseIntroColor: '#0069d9' } };
+            currentColumnObj.content[currentColumnContentIndex][contentIndex] = { type: 'courseObjectives', output: [ { name: 'Course Information', content: 'No information provided yet.'}, { name: 'Course Requirements', content: 'No requirements provided yet.'}, ], class: '', id: '', styles: { courseIntroColor: '#0069d9' }, introVideo: 'https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4' };
         }
 
         const columns = this.state.column;
