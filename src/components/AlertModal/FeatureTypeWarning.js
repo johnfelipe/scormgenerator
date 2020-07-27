f12dd9dc2285a73c766b31abb33cbe995f72ec63
zeejay09 <zeejaybelamide@gmail.com>
2020-07-27 17:31:46 +0800

    creates a feature type change warning modal

---
+++ src/components/AlertModal/FeatureTypeWarning.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Modal } from 'react-bootstrap';
+
+function FeatureTypeWarning(props) {
+
+    const modalShow = props.modalShow;
+
+    const confirmationModal = (
+        <Modal
+            show={modalShow}
+            onHide={() => props.setModalShow(false)}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Warning
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <span>You can only create 1 final quiz per course, do you want to continue?</span>
+            </Modal.Body>
+            <Modal.Footer>
+                <button
+                    className="btn btn-primary"
+                    onClick={() => {
+                        // props.setRemoveFeaures(true);
+                        props.setModalShow(false);
+                    }}
+                >
+                    Yes
+                </button>
+                <button
+                    className="btn btn-danger"
+                    onClick={() => {
+                        // props.setRemoveFeaures(false);
+                        props.setModalShow(false);
+                    }}
+                >
+                    No
+                </button>
+            </Modal.Footer>
+        </Modal>
+    );
+
+    return (
+        <>
+            {confirmationModal}
+        </>
+    );
+}
+
+export default FeatureTypeWarning;
