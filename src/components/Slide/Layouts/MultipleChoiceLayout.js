8f07260c01593a62885d10ae61cbe10a8323d9e3
zeejay09 <zeejaybelamide@gmail.com>
2020-07-29 19:35:41 +0800

    displays explanation to the preview

--- src/components/Slide/Layouts/MultipleChoiceLayout.js
+++ src/components/Slide/Layouts/MultipleChoiceLayout.js
@@ -449,6 +449,18 @@ function MultipleChoiceLayout(props) {
                             </div>
                             {content(item, multipleChoiceClass)}
                             {props.cssApplier(multipleChoiceCss, 'multiple-choice-layout')}
+                            {
+                                item.explanation &&
+                                <>
+                                    <div className="col-md-2"></div>
+                                    <div className="col-md-8 mt-3">
+                                        <p id="question-explanation" className="p-3 rounded">
+                                            {item.explanation}
+                                        </p>
+                                    </div>
+                                    <div className="col-md-2"></div>
+                                </>
+                            }
                         </div>
                     ))
                 :
