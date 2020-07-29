618703bc49261c128417c337c31b3a755ef91f41
zeejay09 <zeejaybelamide@gmail.com>
2020-07-29 19:48:01 +0800

    removes the course border and adds a border-bottom class to the title instead to act as a border between title and subtitle

--- src/components/Slide/Layouts/HomePageLayout.js
+++ src/components/Slide/Layouts/HomePageLayout.js
@@ -16,8 +16,10 @@ function HomePageLayout(props) {
             <div className="home-page-container h-100 w-100 border border-light" style={{ backgroundImage: 'url("' + backgroundImg + '")', backgroundSize: 'cover' }}>
                 <div className="slide">
                     <div id="course-name" className={homePageClass} style={{ background: colorScheme.titleBoxColor}}>
-                        <h2 className="course-header">{title}</h2>
-                    <div className="course-bar" style={{ background: colorHelpers.shade(colorScheme.titleBoxColor, 0.45)}}></div>
+                        <h2 className="course-header border-bottom pb-3" style={{ borderBottomColor: colorHelpers.shade(colorScheme.titleBoxColor, 0.45) }}>
+                            {title}
+                        </h2>
+                    {/* <div className="course-bar" style={{ background: colorHelpers.shade(colorScheme.titleBoxColor, 0.45)}}></div> */}
                     <h3 className="course-sub-header">
                         <span>{subtitle}</span>
                         <br/>
