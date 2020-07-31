f5518698125983358c9d72d1a24bd7e93b4db471
zeejay09 <zeejaybelamide@gmail.com>
2020-07-31 14:54:45 +0800

    adds close button when adding an aswer

--- src/components/Slide/Features/MultipleChoice/MultipleChoiceAccordion.js
+++ src/components/Slide/Features/MultipleChoice/MultipleChoiceAccordion.js
@@ -249,7 +249,7 @@ function MultipleChoiceAccordion(props) {
                                             <div className="multiple-choice-control-button">
                                                 <button
                                                     type="button"
-                                                    className="btn btn-success btn-sm"
+                                                    className="btn btn-success btn-sm mr-1"
                                                     onClick={() => {
                                                         const isEmpty = document.getElementById("answer");
                                                         
@@ -261,6 +261,20 @@ function MultipleChoiceAccordion(props) {
                                                     }}
                                                 >
                                                     <FontAwesomeIcon icon={faArrowAltCircleRight}/>
+                                                </button>
+                                                <button
+                                                    type="button"
+                                                    className="btn btn-danger btn-sm"
+                                                    onClick={() => {
+                                                        const isEmpty = document.getElementById("answer");
+                                                        
+                                                        if (isEmpty.value !== "") {
+                                                            props.setAnswer('');
+                                                            props.setIsAddAnswer(false);
+                                                        }
+                                                    }}
+                                                >
+                                                    <FontAwesomeIcon icon={faTimes}/>
                                                 </button>
                                             </div>
                                         </div>
