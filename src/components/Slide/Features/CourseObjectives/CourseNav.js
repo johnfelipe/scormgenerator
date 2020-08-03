895c5772e34280bf72671e5904963a00e01f1134
zeejay09 <zeejaybelamide@gmail.com>
2020-08-03 14:51:46 +0800

    creates the course navigation content

---
+++ src/components/Slide/Features/CourseObjectives/CourseNav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CourseNav.css';
+
+function CourseNav() {
+
+    return (
+        <div id="course-navigation" className="course-navigation-container h-100 w-100">
+            
+        </div>
+    );    
+}
+
+export default CourseNav;
