d246770ddefbf872d63bea17c0f012603b9a7c34
zeejay09 <zeejaybelamide@gmail.com>
2020-08-04 16:15:59 +0800

    creates am uploadFIles method in the gallery service

--- src/services/gallery.service.js
+++ src/services/gallery.service.js
@@ -2,10 +2,22 @@ import API from "../utils/api";
 
 export const galleryService = {
     getAllFiles,
+    uploadFiles,
 };
 
 async function getAllFiles() {
     const response = await API.get('/user/1/files?pageNo=0&pageSize=10&sortBy=updated',);
+    return handleResponse(response);
+}
+
+async function uploadFiles(data) {
+    const config = {
+        headers: {
+            'content-type': 'multipart/form-data'
+        }
+    }
+
+    const response = await API.post('/uploadFile', data, config);
     return handleResponse(response);
 }
 
