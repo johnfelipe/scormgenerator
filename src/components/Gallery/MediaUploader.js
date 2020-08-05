f018a58b098c0b149b09e4bc5f35f83a8a3fd16a
zeejay09 <zeejaybelamide@gmail.com>
2020-08-05 14:32:53 +0800

    changes the class like style of coding to hooks style of coding

--- src/components/Gallery/MediaUploader.js
+++ src/components/Gallery/MediaUploader.js
@@ -1,22 +1,16 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Alert from 'react-bootstrap/Alert';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { galleryService } from '../../services';
 
 // https://codepen.io/hartzis/pen/VvNGZP
-class MediaUploader extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showSuccessMsg: false,
-            showErrorMsg: false,
-        };
-        this.handleImageChange = this.handleImageChange.bind(this);
-        this.clearSuccessMessage = this.clearSuccessMessage.bind(this);
-    }
+function MediaUploader(props) {
 
-    handleImageChange = (e) => {
+    const [showSuccessMsg, setShowSuccessMsg] = useState(false);
+    const [showErrorMsg, setShowErrorMsg] = useState(false);
+
+    const handleImageChange = (e) => {
         e.preventDefault();
         let files = e.target.files;
         console.log(files);
@@ -35,19 +29,15 @@ class MediaUploader extends Component {
                 .then(
                     fileObject => {
                         console.log(fileObject);
-                        this.props.setMediaFiles(fileObject);
+                        props.setMediaFiles(fileObject);
                     },
                     error => console.log(error)
                 );
 
-                this.setState({
-                    showSuccessMsg: true,
-                })
+                setShowSuccessMsg(true);
             } else {
                 document.getElementById("inputGroupFile01").value = "";
-                this.setState({
-                    showErrorMsg: true,
-                })
+                setShowErrorMsg(true);
             }
             
 
@@ -65,66 +55,64 @@ class MediaUploader extends Component {
             // reader.readAsDataURL(files[fileIndex])
             // reader.onloadend = () => {
             //     fileObject.url = reader.result;
-            //     this.props.setMediaFiles(fileObject);
+            //     props.setMediaFiles(fileObject);
             // }
         });
     }
 
-    clearSuccessMessage = () => {
+    const clearSuccessMessage = () => {
         setTimeout(
             function() {
-                this.setState({showSuccessMsg: false, showErrorMsg: false});
-            }
-            .bind(this),
+                
+                setShowSuccessMsg(false);
+                setShowErrorMsg(false);
+            },
             5000
         );
     }
 
-    render() {
-
-        return (
-            <div className="row mt-5">
-                <div className="col-md-4"></div>
-                <div className="text-center col-md-4">
-                    <div className="input-group">
-                        <div className="custom-file">
-                            <input
-                                type="file"
-                                className="custom-file-input"
-                                id="inputGroupFile01"
-                                aria-describedby="inputGroupFileAddon01"
-                                onChange={this.handleImageChange}
-                                onBlur={this.clearSuccessMessage}
-                                // multiple
-                            />
-                            <label className="custom-file-label pr-6" htmlFor="inputGroupFile01">
-                                {
-                                    this.state.showSuccessMsg ?
-                                        <span className="text-success">File are successfully uploaded!</span>
+    return (
+        <div className="row mt-5">
+            <div className="col-md-4"></div>
+            <div className="text-center col-md-4">
+                <div className="input-group">
+                    <div className="custom-file">
+                        <input
+                            type="file"
+                            className="custom-file-input"
+                            id="inputGroupFile01"
+                            aria-describedby="inputGroupFileAddon01"
+                            onChange={handleImageChange}
+                            onBlur={clearSuccessMessage}
+                            // multiple
+                        />
+                        <label className="custom-file-label pr-6" htmlFor="inputGroupFile01">
+                            {
+                                showSuccessMsg ?
+                                    <span className="text-success">File are successfully uploaded!</span>
+                                :
+                                    showErrorMsg ?
+                                        <span className="text-danger">File are not uploaded!</span>
                                     :
-                                        this.state.showErrorMsg ?
-                                            <span className="text-danger">File are not uploaded!</span>
-                                        :
-                                            <span>Choose file/s</span>
-                                }
-                            </label>
-                        </div>
-                    </div>
-                    <div id="success-message" className={this.state.showSuccessMsg ? 'fadeIn mt-5' : 'fadeOut mt-5'}>
-                        <Alert variant='success'>
-                            <span><FontAwesomeIcon icon={faCheck}/> Successfully uploaded!</span>
-                        </Alert>
-                    </div>
-                    <div id="error-message" className={this.state.showErrorMsg ? 'fadeIn mt-5' : 'fadeOut mt-5'}>
-                        <Alert variant='danger'>
-                            <span><FontAwesomeIcon icon={faTimes}/> Cannot upload selected file!</span>
-                        </Alert>
+                                        <span>Choose file/s</span>
+                            }
+                        </label>
                     </div>
                 </div>
-                <div className="col-md-4"></div>
+                <div id="success-message" className={showSuccessMsg ? 'fadeIn mt-5' : 'fadeOut mt-5'}>
+                    <Alert variant='success'>
+                        <span><FontAwesomeIcon icon={faCheck}/> Successfully uploaded!</span>
+                    </Alert>
+                </div>
+                <div id="error-message" className={showErrorMsg ? 'fadeIn mt-5' : 'fadeOut mt-5'}>
+                    <Alert variant='danger'>
+                        <span><FontAwesomeIcon icon={faTimes}/> Cannot upload selected file!</span>
+                    </Alert>
+                </div>
             </div>
-        )
-    }
+            <div className="col-md-4"></div>
+        </div>
+    );
 
 }
 
