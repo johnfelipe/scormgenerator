bed04596edeee9a2d53489e0cd494d60b9c766f3
zeejay09 <zeejaybelamide@gmail.com>
2020-08-06 14:29:02 +0800

    refactors the default properties in grid size 5

--- src/components/Handlers/SlideHandler.js
+++ src/components/Handlers/SlideHandler.js
@@ -778,7 +778,7 @@ class SlideHandler extends Component {
                             class: '',
                             id: ''
                         };
-                        
+
                         currentColumns[key].content.subColumnTwo.push(currentContent);
                         this.setState({
                             column: currentColumns,
@@ -801,7 +801,13 @@ class SlideHandler extends Component {
                     const currentFeatures = this.state.features;
 
                     if (currentFeatures[source.index]['type'] === 'contentArea') {
-                        let currentContent = { type: currentFeatures[source.index]['type'], output: '<span>This content will show up directly in its container.</span>', class: '', id: '' };
+                        let currentContent = {
+                            type: currentFeatures[source.index]['type'],
+                            output: '<span>This content will show up directly in its container.</span>',
+                            class: '',
+                            id: ''
+                        };
+
                         currentColumns[key].content.subColumnOne.push(currentContent);
                         this.setState({
                             column: currentColumns,
@@ -810,7 +816,13 @@ class SlideHandler extends Component {
                             activeContentIndex: (currentColumns[key].content.subColumnOne.length - 1),
                         })
                     } else if (currentFeatures[source.index]['type'] === 'audio') {
-                        let currentContent = { type: currentFeatures[source.index]['type'], output: '', class: '', id: ''  };
+                        let currentContent = {
+                            type: currentFeatures[source.index]['type'],
+                            output: '',
+                            class: '',
+                            id: ''
+                        };
+
                         currentColumns[key].content.subColumnOne.push(currentContent);
                         this.setState({
                             column: currentColumns,
@@ -832,7 +844,13 @@ class SlideHandler extends Component {
                     const currentFeatures = this.state.features;
 
                     if (currentFeatures[source.index]['type'] === 'contentArea') {
-                        let currentContent = { type: currentFeatures[source.index]['type'], output: '<span>This content will show up directly in its container.</span>', class: '', id: '' };
+                        let currentContent = {
+                            type: currentFeatures[source.index]['type'],
+                            output: '<span>This content will show up directly in its container.</span>',
+                            class: '',
+                            id: ''
+                        };
+
                         currentColumns[key].content.subColumnTwo.push(currentContent);
                         this.setState({
                             column: currentColumns,
@@ -841,7 +859,13 @@ class SlideHandler extends Component {
                             activeContentIndex: (currentColumns[key].content.subColumnTwo.length - 1),
                         })
                     } else if (currentFeatures[source.index]['type'] === 'audio') {
-                        let currentContent = { type: currentFeatures[source.index]['type'], output: '', class: '', id: ''  };
+                        let currentContent = {
+                            type: currentFeatures[source.index]['type'],
+                            output: '',
+                            class: '',
+                            id: ''
+                        };
+
                         currentColumns[key].content.subColumnTwo.push(currentContent);
                         this.setState({
                             column: currentColumns,
@@ -863,7 +887,13 @@ class SlideHandler extends Component {
                     const currentFeatures = this.state.features;
 
                     if (currentFeatures[source.index]['type'] === 'contentArea') {
-                        let currentContent = { type: currentFeatures[source.index]['type'], output: '<span>This content will show up directly in its container.</span>', class: '', id: '' };
+                        let currentContent = {
+                            type: currentFeatures[source.index]['type'],
+                            output: '<span>This content will show up directly in its container.</span>',
+                            class: '',
+                            id: ''
+                        };
+
                         currentColumns[key].content.subColumnThree.push(currentContent);
                         this.setState({
                             column: currentColumns,
@@ -872,7 +902,13 @@ class SlideHandler extends Component {
                             activeContentIndex: (currentColumns[key].content.subColumnThree.length - 1),
                         })
                     } else if (currentFeatures[source.index]['type'] === 'audio') {
-                        let currentContent = { type: currentFeatures[source.index]['type'], output: '', class: '', id: ''  };
+                        let currentContent = {
+                            type: currentFeatures[source.index]['type'],
+                            output: '',
+                            class: '',
+                            id: ''
+                        };
+                        
                         currentColumns[key].content.subColumnThree.push(currentContent);
                         this.setState({
                             column: currentColumns,
