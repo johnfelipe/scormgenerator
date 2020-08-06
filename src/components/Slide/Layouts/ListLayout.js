8229fb8f834b2818e59d0a37aebc6626fd90c207
zeejay09 <zeejaybelamide@gmail.com>
2020-08-06 16:27:56 +0800

    renames the class to ListLayout

--- src/components/Slide/Layouts/ListLayout.js
+++ src/components/Slide/Layouts/ListLayout.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-function HomePageLayout() {
+function ListLayout(props) {
 
+    const { output } = props;
+    
     return (
         <div id="list-layout">
-            
+            <div className="list-container h-100 w-100 border border-light">
+                <span>Sample</span>
+            </div>
         </div>
     );
 }
 
-export default HomePageLayout;
+export default ListLayout;
