64370407f57f5ac6387c475db91359427644bc67
zeejay09 <zeejaybelamide@gmail.com>
2020-08-10 18:26:15 +0800

    creates another component to solve the infinite loop issue

---
+++ src/components/Slide/Features/List/ListModals.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { Modal } from 'react-bootstrap';
+
+function ListModals(props) {
+
+    const { item, index, styles } = props;
+    const [modalShow, setModalShow] = useState(false);
+
+    const buttonModal = (
+        <Modal
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title>
+                    {item.name}
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {item.content}
+            </Modal.Body>
+        </Modal>
+    );
+
+    return (
+        <>
+            {styles.btnWidth === 0 ?
+                <div
+                    key={'list-button-object-' + index}
+                    className="list-modal-button-object"
+                >
+                    <button
+                        type="button"
+                        className={"btn btn-secondary mb-2 w-100 " + styles.btnLabelAlignment}
+                        style={{ background: styles.btnColor }}
+                        onClick={() => {
+                            setModalShow(true);
+                        }}
+                    >
+                        {item.name}
+                    </button>
+                </div>
+            :
+                <div
+                    key={'list-button-object-' + index}
+                    className="list-modal-button-object"
+                >
+                    <button
+                        type="button"
+                        className={"btn btn-secondary mb-2 " + styles.btnLabelAlignment}
+                        style={{ width: styles.btnWidth + '%', background: styles.btnColor }}
+                        onClick={() => {
+                            setModalShow(true);
+                        }}
+                    >
+                        <span>
+                            {item.name}
+                        </span>
+                    </button>
+                </div>
+            }
+            {buttonModal}
+        </>
+    );
+}
+
+export default ListModals;
