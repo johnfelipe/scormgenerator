ee18b9ce642d3114f82ea7d0e6c239091a24b4fb
zeejay09 <zeejaybelamide@gmail.com>
2020-08-11 13:57:05 +0800

    displays the paragraph if added by the user

--- src/components/Slide/Layouts/VideoLayout.js
+++ src/components/Slide/Layouts/VideoLayout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactHtmlParser from 'react-html-parser';
 
 function VideoLayout(props) {
 
@@ -8,9 +9,14 @@ function VideoLayout(props) {
         <div id="video-layout">
             <div className="video-container h-100 w-100 border border-light">
                 {output.url !== '' ?
-                    <video className="video-layout-video h-100 w-100" controls src={output.url}>
-                        Your browser does not support the video tag.
-                    </video>
+                    <>
+                        <video className="video-layout-video h-100 w-100" controls src={output.url}>
+                            Your browser does not support the video tag.
+                        </video>
+                        <div className="mt-3 p-3">
+                            {ReactHtmlParser(output.paragraph)}
+                        </div>
+                    </>
                 :
                     <span>No video added yet.</span>
                 }
