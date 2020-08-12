c93314086d24128cce8e8b18aded26fe94e7d4b3
zeejay09 <zeejaybelamide@gmail.com>
2020-08-12 16:58:21 +0800

    adds a dynamic theme color

--- src/components/Slide/Layouts/DragDropLayout.js
+++ src/components/Slide/Layouts/DragDropLayout.js
@@ -11,8 +11,15 @@ function DragDropLayout(props) {
         return (
             <div className="col-md-12 pl-3 row m-0">
                 <div className="col-md-6 text-center">
-                    <ul class="options text-break">
-                        <li class="title">Options</li>
+                    <ul
+                        className="options text-break"
+                        ref={(el) => {
+                            if (el) {
+                                el.style.setProperty('border-color', dragDropStyles.themeColor, 'important');
+                            }
+                        }}
+                    >
+                        <li className="title" style={{ background: dragDropStyles.themeColor }}>Options</li>
                         {item.options.map((option, optionIndex) => (
                             <li key={'drag-drop-option-' + optionIndex}>{option.name}</li>
                         ))}
@@ -24,7 +31,7 @@ function DragDropLayout(props) {
                             <li key={"drag-drop-question-answer" + questionIndex}>
                                 <div className="drag-drop mb-2 m-0 row">
                                     <div className="drag-drop-choice-text font-15">
-                                        <span class="target" data-accept={question.answer}>&nbsp;</span>
+                                        <span className="target" data-accept={question.answer}>&nbsp;</span>
                                         <span>,&nbsp;{question.question}</span>
                                     </div>
                                 </div>
@@ -41,8 +48,8 @@ function DragDropLayout(props) {
             {dragDrop.length > 0 ?
                 dragDrop.map((item, itemIndex) => (
                     <div key={"drag-drop-question-" + itemIndex} className={"question-group row mb-4 " + dragDropClass}>
-                        <div className="col-md-12">
-                            <p className="font-20"><span>{(itemIndex+1) + '. ' + item.instruction}</span></p>
+                        <div className="col-md-12 mb-3 p-4 text-white" style={{ background: dragDropStyles.themeColor }}>
+                            <p className="font-20 m-0"><span>{(itemIndex+1) + '. ' + item.instruction}</span></p>
                         </div>
                         {content(item)}
                         {props.cssApplier(dragDropCss, 'drag-drop-layout')}
