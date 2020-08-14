c3b9ada8498e53393287b2b30fbc7a37fab8a3da
zeejay09 <zeejaybelamide@gmail.com>
2020-08-14 18:57:42 +0800

    creates ImageLayout component

---
+++ src/components/Slide/Layouts/ImageLayout.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactHtmlParser from 'react-html-parser';
+
+function ImageLayout(props) {
+
+    const { output } = props;
+
+    return (
+        <div id="video-layout">
+            <div className="video-container h-100 w-100 border border-light">
+                {output.url !== '' ?
+                    <>
+                        <video className="video-layout-video h-100 w-100" controls src={output.url}>
+                            Your browser does not support the video tag.
+                        </video>
+                        {output.paragraph &&
+                            <div className="mt-3 p-3">
+                                {ReactHtmlParser(output.paragraph)}
+                            </div>
+                        }
+                    </>
+                :
+                    <span>No video added yet.</span>
+                }
+            </div>
+        </div>
+    );
+}
+
+export default ImageLayout;
