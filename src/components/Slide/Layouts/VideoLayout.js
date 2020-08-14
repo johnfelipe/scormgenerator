3b8c815aaaf0468c3e97843fb789a2a0742b078b
zeejay09 <zeejaybelamide@gmail.com>
2020-08-14 18:56:52 +0800

    replaces the html5 video player with the react video player to utilize the vtt uploaded by the user

--- src/components/Slide/Layouts/VideoLayout.js
+++ src/components/Slide/Layouts/VideoLayout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactHtmlParser from 'react-html-parser';
+import { Player, ControlBar, ClosedCaptionButton } from 'video-react';
 
 function VideoLayout(props) {
 
@@ -10,12 +11,29 @@ function VideoLayout(props) {
             <div className="video-container h-100 w-100 border border-light">
                 {output.url !== '' ?
                     <>
-                        <video className="video-layout-video h-100 w-100" controls src={output.url}>
+                        {/* <video className="video-layout-video h-100 w-100" controls src={output.url}>
                             Your browser does not support the video tag.
-                        </video>
-                        <div className="mt-3 p-3">
-                            {ReactHtmlParser(output.paragraph)}
-                        </div>
+                        </video> */}
+                        <Player
+                            className="video-layout-video h-100 w-100"
+                        >
+                            <source src={output.url} />
+                            <track
+                                kind="captions"
+                                src={output.vtt.url && output.vtt.url}
+                                srcLang="en"
+                                label="English"
+                                default
+                            />
+                            <ControlBar autoHide={true}>
+                                <ClosedCaptionButton order={7} />
+                            </ControlBar>
+                        </Player>
+                        {output.paragraph &&
+                            <div className="mt-3 p-3">
+                                {ReactHtmlParser(output.paragraph)}
+                            </div>
+                        }
                     </>
                 :
                     <span>No video added yet.</span>
