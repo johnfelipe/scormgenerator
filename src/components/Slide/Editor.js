a3cf546454e7949546b9c1159c01bbedf07749e1
zeejay09 <zeejaybelamide@gmail.com>
2020-08-17 16:57:32 +0800

    creates another condition to render the List editor interface component

--- src/components/Slide/Editor.js
+++ src/components/Slide/Editor.js
@@ -11,7 +11,7 @@ import Video from './Features/Video';
 import DragDrop from './Features/DragDrop/DragDrop';
 import Card from './Features/Card/Card';
 import Image from './Features/Image';
-
+import List from './Features/List/List';
 class Editor extends Component {
 
     constructor(props) {
@@ -235,6 +235,22 @@ class Editor extends Component {
                     setShowCssEditor={this.props.setShowCssEditor}
                     resetFeature={this.props.resetFeature}
                     setShowEditor={this.props.setShowEditor}
+                />
+            );
+        } else if (this.props.feature === "list") {
+            editorContent = (
+                <List
+                    setColumn={this.props.setColumn}
+                    deleteFeature={this.props.deleteFeature}
+                    contentIndex={this.props.contentIndex}
+                    setShowEditor={this.props.setShowEditor}
+                    currentColumn={this.props.currentColumn}
+                    currentColumnContentIndex={this.props.currentColumnContentIndex}
+                    setFeatureId={this.props.setFeatureId}
+                    setFeatureClass={this.props.setFeatureClass}
+                    setShowCssEditor={this.props.setShowCssEditor}
+                    setShowTextEditor={this.props.setShowTextEditor}
+                    resetFeature={this.props.resetFeature}
                 />
             );
         } else {
