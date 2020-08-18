d974886b7d8c96a4a80bd17211e520aee3191a58
zeejay09 <zeejaybelamide@gmail.com>
2020-08-18 16:57:57 +0800

    creates layout for content area

---
+++ src/components/Slide/Layouts/ContentAreaLayout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactHtmlParser from 'react-html-parser';
+
+function ContentAreaLayout(props) {
+
+    const { output, style, css } = props;
+    
+    return (
+        <div id="content-area-layout" style={{ backgroundImage: 'url("' + style.backgroundImg.url + '")', backgroundSize: 'cover' }}>
+            <div className="content-area-container h-100 w-100 border border-light p-3 ">
+                {ReactHtmlParser(output)}
+                {props.cssApplier(css, 'drag-drop-layout')}
+            </div>
+        </div>
+    );
+}
+
+export default ContentAreaLayout;
