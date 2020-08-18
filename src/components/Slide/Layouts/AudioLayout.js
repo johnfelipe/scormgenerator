2ec8f800842be21f295bec6493d957785e907985
zeejay09 <zeejaybelamide@gmail.com>
2020-08-18 17:04:56 +0800

    creates audio layout component

--- src/components/Slide/Layouts/AudioLayout.js
+++ src/components/Slide/Layouts/AudioLayout.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactHtmlParser from 'react-html-parser';
 
-function ContentAreaLayout(props) {
+function AudioLayout(props) {
 
     const { output, style, css } = props;
     
     return (
-        <div id="content-area-layout" style={{ backgroundImage: 'url("' + style.backgroundImg.url + '")', backgroundSize: 'cover' }}>
+        <div id="audio-layout" style={{ backgroundImage: 'url("' + style.backgroundImg.url + '")', backgroundSize: 'cover' }}>
             <div className="content-area-container h-100 w-100 border border-light p-3 ">
                 {ReactHtmlParser(output)}
-                {props.cssApplier(css, 'content-area-layout')}
+                {props.cssApplier(css, 'audio-layout')}
             </div>
         </div>
     );
 }
 
-export default ContentAreaLayout;
+export default AudioLayout;
