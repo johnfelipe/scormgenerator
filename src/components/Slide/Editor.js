e9931818e8fdf9e8b8369d3ca36ed091927de619
zeejay09 <zeejaybelamide@gmail.com>
2020-08-19 16:54:29 +0800

    modifies the background image uploader of ListModal feature

--- src/components/Slide/Editor.js
+++ src/components/Slide/Editor.js
@@ -175,6 +175,7 @@ class Editor extends Component {
                     setShowCssEditor={this.props.setShowCssEditor}
                     setShowTextEditor={this.props.setShowTextEditor}
                     resetFeature={this.props.resetFeature}
+                    setMediaFiles={this.setMediaFiles}
                 />
             );
         } else if (this.props.feature === "video") {
