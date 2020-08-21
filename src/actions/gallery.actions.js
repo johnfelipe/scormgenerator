5ad1d7433d2c9f7499782592334dd1b2b9dbac1c
zeejay09 <zeejaybelamide@gmail.com>
2020-08-21 17:03:37 +0800

    creates gallery actions

---
+++ src/actions/gallery.actions.js
@@ -0,0 +1,74 @@
+import { galleryConstants } from '../constants';
+import { galleryService } from '../services';
+import { alertActions } from './';
+// import { history } from '../helpers';
+
+export const galleryActions = {
+    getAllFiles,
+    uploadFiles,
+    getFile,
+};
+
+function getAllFiles() {
+    return dispatch => {
+        dispatch(request());
+
+        galleryService.getAllFiles()
+            .then(
+                files => { 
+                    dispatch(success(files));
+                },
+                error => {
+                    dispatch(failure(error.toString()))
+                }
+            );
+    };
+
+    function request(files) { return { type: galleryConstants.REQUEST, files } }
+    function success(files) { return { type: galleryConstants.GETALL_FILES_SUCCESS, files } }
+    function failure(error) { return { type: galleryConstants.ERROR, error } }
+}
+
+function uploadFiles(data) {
+    return dispatch => {
+        dispatch(request(data));
+
+        galleryService.uploadFiles(data)
+            .then(
+                file => { 
+                    dispatch(success(file));
+                    dispatch(alertActions.success('File uploaded successfully'));
+                },
+                error => {
+                    dispatch(failure(error.toString()));
+                    dispatch(alertActions.error(error.toString()));
+                }
+            );
+    };
+
+    function request(file) { return { type: galleryConstants.REQUEST, file } }
+    function success(file) { return { type: galleryConstants.UPLOAD_SUCCESS, file } }
+    function failure(error) { return { type: galleryConstants.ERROR, error } }
+}
+
+function getFile(id) {
+    return dispatch => {
+        dispatch(request(id));
+
+        galleryService.getFile(id)
+            .then(
+                file => { 
+                    dispatch(success(file));
+                    dispatch(alertActions.success('File fetched successfully'));
+                },
+                error => {
+                    dispatch(failure(error.toString()));
+                    dispatch(alertActions.error(error.toString()));
+                }
+            );
+    };
+
+    function request(id) { return { type: galleryConstants.REQUEST, id } }
+    function success(file) { return { type: galleryConstants.GET_FILE_SUCCESS, file } }
+    function failure(error) { return { type: galleryConstants.ERROR, error } }
+}
