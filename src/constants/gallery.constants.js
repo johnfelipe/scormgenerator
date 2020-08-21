6896270fa676dbf21a5d80def3bb412fc1c96890
zeejay09 <zeejaybelamide@gmail.com>
2020-08-21 17:03:46 +0800

    creates gallery constants

---
+++ src/constants/gallery.constants.js
@@ -0,0 +1,15 @@
+export const galleryConstants = {
+    // GENERAL
+    REQUEST: 'REQUEST',
+    ERROR: 'ERROR',
+    CLEAR: 'CLEAR',
+
+    // GET ALL
+    GETALL_FILES_SUCCESS: 'GETALL_FILES_SUCCESS',
+
+    // UPLOAD FILE
+    UPLOAD_SUCCESS: 'UPLOAD_SUCCESS',
+
+    // GET INDICIDUAL FILE
+    GET_FILE_SUCCESS: 'GET_FILE_SUCCESS',
+};
