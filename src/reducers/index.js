16dc2996e9a0d9fdb610838d79bb43a298a76e8d
zeejay09 <zeejaybelamide@gmail.com>
2020-08-24 17:25:11 +0800

    creates slide reducer component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,12 +5,14 @@ import { alert } from './alert.reducer';
 import { course } from './course.reducer';
 import { gallery } from './gallery.reducer';
 import { lesson } from './lesson.reducer';
+import { slide } from './slide.reducer';
 
 const rootReducer = combineReducers({
     alert,
     course,
     gallery,
     lesson,
+    slide,
 });
 
 export default rootReducer;
---
+++ src/reducers/slide.reducer.js
@@ -0,0 +1,55 @@
+import { slideContants } from '../constants';
+const initialState = {
+    currentSlide: {},
+    slides: [],
+    slideColumns: [],
+}
+
+export function slide(state = initialState, action) {
+    switch (action.type) {
+        case slideContants.REQUEST:
+            return {
+                loading: true
+            };
+
+        case slideContants.CREATE_SUCCESS:
+            return {
+                ...state,
+                currentSlide: action.slide
+            };
+
+        case slideContants.GETALL_SLIDE_SUCCESS:
+            return {
+                ...state,
+                slides: action.slides
+            };
+
+        case slideContants.GET_SLIDE_SUCCESS:
+            return {
+                ...state,
+                currentSlide: action.slide
+            };
+
+        case slideContants.GET_SLIDE_COLUMNS_SUCCESS:
+            const { loading, ...stateCopy } = state;
+
+            return {
+                ...stateCopy,
+                slideColumns: action.slides
+            };
+
+        case slideContants.UPDATE_SUCCESS:
+            return {
+                ...state,
+                currentSlide: action.slide
+            };
+    
+        case slideContants.ERROR:
+            return { 
+                error: action.error
+            };
+
+        default:
+            return state
+    }
+}
