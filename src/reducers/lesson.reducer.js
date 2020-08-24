aa6509480f9efbee09a9af5873a286def854f040
zeejay09 <zeejaybelamide@gmail.com>
2020-08-24 13:46:47 +0800

    creates lesson reducer

---
+++ src/reducers/lesson.reducer.js
@@ -0,0 +1,43 @@
+import { lessonContants } from '../constants';
+const initialState = {
+    currentLesson: {},
+    lessons: [],
+    lessonSlides: [],
+}
+
+export function lesson(state = initialState, action) {
+    switch (action.type) {
+        case lessonContants.REQUEST:
+            return {
+                loading: true
+            };
+
+        case lessonContants.CREATE_SUCCESS:
+            return {
+                currentLesson: action.course
+            };
+
+        case lessonContants.GETALL_LESSON_SUCCESS:
+            return {
+                lessons: action.courses
+            };
+
+        case lessonContants.GET_LESSON_SUCCESS:
+            return {
+                currentLesson: action.course
+            };
+
+        case lessonContants.GET_LESSON_SLIDES_SUCCESS:
+            return {
+                lessonSlides: action.lessons
+            };
+    
+        case lessonContants.ERROR:
+            return { 
+                error: action.error
+            };
+
+        default:
+            return state
+    }
+}
