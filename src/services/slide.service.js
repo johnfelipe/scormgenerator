a45bc46a497ade9bd8997a2b426671e67b1cbdcc
zeejay09 <zeejaybelamide@gmail.com>
2020-08-24 16:07:15 +0800

    modifies slide service and adds necessary methods

--- src/services/slide.service.js
+++ src/services/slide.service.js
@@ -2,10 +2,34 @@ import API from "../utils/api";
 
 export const slideService = {
     createSlide,
+    getAllSlides,
+    getSlide,
+    updateSlide,
+    getSlideColumns,
 };
 
 async function createSlide(slideObj) {
     const response = await API.post('/slides', slideObj);
+    return handleResponse(response);
+}
+
+async function getAllSlides() {
+    const response = await API.get('/slides',);
+    return handleResponse(response);
+}
+
+async function getSlide(id) {
+    const response = await API.get('/slides/' + id,);
+    return handleResponse(response);
+}
+
+async function updateSlide(id) {
+    const response = await API.put('/slides/' + id,);
+    return handleResponse(response);
+}
+
+async function getSlideColumns(id) {
+    const response = await API.get('/slides/' + id + '/columns?pageNo=0&pageSize=10000&sortBy=clid',);
     return handleResponse(response);
 }
 
