3e87ac8d1a1067927a85c2e3d9d93cd7b5ef9a4b
zeejay09 <zeejaybelamide@gmail.com>
2020-08-26 18:33:37 +0800

    creates column actions

---
+++ src/actions/column.actions.js
@@ -0,0 +1,31 @@
+import { columnContants } from '../constants';
+import { columnService } from '../services';
+// import { alertActions } from './';
+// import { history } from '../helpers';
+
+export const slideActions = {
+    createColumn,
+};
+
+function createColumn(data) {
+    return dispatch => {
+        dispatch(request(data));
+
+        columnService.createColumn(data)
+            .then(
+                column => { 
+                    dispatch(success(column));
+                    // dispatch(alertActions.success('Slide created successfully'));
+                },
+                error => {
+                    dispatch(failure(error.toString()));
+                    // dispatch(alertActions.error(error.toString()));
+                    console.log(error);
+                }
+            );
+    };
+
+    function request(column) { return { type: columnContants.REQUEST, column } }
+    function success(column) { return { type: columnContants.CREATE_SUCCESS, column } }
+    function failure(error) { return { type: columnContants.ERROR, error } }
+}
--- src/actions/index.js
+++ src/actions/index.js
@@ -3,3 +3,4 @@ export * from './course.actions';
 export * from './gallery.actions';
 export * from './lesson.actions';
 export * from './slide.actions';
+export * from './column.actions';
