57d0920d34774859da9ceb02a7cb19e49edf275f
zeejay09 <zeejaybelamide@gmail.com>
2020-08-26 18:33:26 +0800

    creates column constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,3 +3,4 @@ export * from './course.constants';
 export * from './gallery.constants';
 export * from './lesson.constants';
 export * from './slide.constants';
+export * from './column.constants';
