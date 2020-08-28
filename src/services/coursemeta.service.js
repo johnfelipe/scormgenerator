d83d8dfd0423fef10d8a78ae87825d52cf204405
zeejay09 <zeejaybelamide@gmail.com>
2020-08-28 22:42:42 +0800

    creates updateCoursemeta and modifies the deleteCoursemeta to avoid getting errors

--- src/services/coursemeta.service.js
+++ src/services/coursemeta.service.js
@@ -2,6 +2,7 @@ import API from "../utils/api";
 
 export const coursemetaService = {
     createCoursemeta,
+    updateCoursemeta,
     deleteCoursemeta,
     getAllCoursemeta,
     getCoursemeta,
@@ -13,8 +14,22 @@ async function createCoursemeta(coursemetaObj) {
     return handleResponse(response);
 }
 
+async function updateCoursemeta(coursemetaObj, id) {
+    const response = await API.put('/coursesmeta/' + id, coursemetaObj);
+    return handleResponse(response);
+}
+
 async function deleteCoursemeta(id) {
-    const response = await API.delete('/coursesmeta/' + id,);
+    let response = '';
+    try {
+        response = await API.delete('/coursesmeta/' + id,);
+    } catch (error) {
+        console.log(error);
+        response = {
+            data: 'Something went wrong.',
+        };
+    }
+
     return handleResponse(response);
 }
 
