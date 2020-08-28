7393158d629c296b21f80edb5f23291b2d9b3e3b
zeejay09 <zeejaybelamide@gmail.com>
2020-08-28 17:31:08 +0800

    comments the setMediaFiles because it serves no purpose anymore, will continue to test for residual codes that are affected by this change

--- src/components/Slide/Editor.js
+++ src/components/Slide/Editor.js
@@ -24,8 +24,8 @@ class Editor extends Component {
         
         this.deleteFeature = this.deleteFeature.bind(this);
         this.radioClick = this.radioClick.bind(this);
-        this.handleFileUpload = this.handleFileUpload.bind(this);
-        this.setMediaFiles = this.setMediaFiles.bind(this);
+        // this.handleFileUpload = this.handleFileUpload.bind(this);
+        // this.setMediaFiles = this.setMediaFiles.bind(this);
     }
 
     deleteFeature = () => {
@@ -41,42 +41,42 @@ class Editor extends Component {
         })
     }
 
-    handleFileUpload = (e) => {
-        let files = e.target.files;
+    // handleFileUpload = (e) => {
+    //     let files = e.target.files;
 
-        this.setState({
-            showSuccessMsg: true,
-        })
+    //     this.setState({
+    //         showSuccessMsg: true,
+    //     })
 
-        // eslint-disable-next-line
-        Object.keys(files).map((fileIndex) => {
+    //     // eslint-disable-next-line
+    //     Object.keys(files).map((fileIndex) => {
 
-            let reader = new FileReader();
+    //         let reader = new FileReader();
 
-            const fileObject = {
-                name: files[fileIndex].name.split(".")[0],
-                extension: files[fileIndex].name.split(".")[1],
-                size: files[fileIndex].size,
-                type: files[fileIndex].type,
-                lastModified: files[fileIndex].lastModified,
-                lastModifiedDate: files[fileIndex].lastModifiedDate,
-            };
+    //         const fileObject = {
+    //             name: files[fileIndex].name.split(".")[0],
+    //             extension: files[fileIndex].name.split(".")[1],
+    //             size: files[fileIndex].size,
+    //             type: files[fileIndex].type,
+    //             lastModified: files[fileIndex].lastModified,
+    //             lastModifiedDate: files[fileIndex].lastModifiedDate,
+    //         };
 
-            reader.readAsDataURL(files[fileIndex])
-            reader.onloadend = () => {
-                fileObject.dataUrl = reader.result;
-                this.setMediaFiles(fileObject);
-            }
-        });
-    }
+    //         reader.readAsDataURL(files[fileIndex])
+    //         reader.onloadend = () => {
+    //             fileObject.dataUrl = reader.result;
+    //             this.setMediaFiles(fileObject);
+    //         }
+    //     });
+    // }
 
-    setMediaFiles = (fileObject) => {
-        const mediaFile = fileObject;
-        let mediaFiles = [...this.props.mediaFilesObject, mediaFile];
+    // setMediaFiles = (fileObject) => {
+    //     const mediaFile = fileObject;
+    //     let mediaFiles = [...this.props.mediaFilesObject, mediaFile];
 
-        this.props.setMediaFilesObject(mediaFiles);
-        this.props.addMediaFiles(mediaFiles);
-    }
+    //     this.props.setMediaFilesObject(mediaFiles);
+    //     this.props.addMediaFiles(mediaFiles);
+    // }
 
     render() {
         let editorContent;
@@ -93,7 +93,7 @@ class Editor extends Component {
                     setFeatureId={this.props.setFeatureId}
                     setFeatureClass={this.props.setFeatureClass}
                     resetFeature={this.props.resetFeature}
-                    setMediaFiles={this.setMediaFiles}
+                    // setMediaFiles={this.setMediaFiles}
                 />
             );
         } else if (this.props.feature === "contentArea") {
@@ -109,7 +109,7 @@ class Editor extends Component {
                     setFeatureClass={this.props.setFeatureClass}
                     setShowCssEditor={this.props.setShowCssEditor}
                     resetFeature={this.props.resetFeature}
-                    setMediaFiles={this.setMediaFiles}
+                    // setMediaFiles={this.setMediaFiles}
                 />
             );
         } else if (this.props.feature === "multipleChoice") {
@@ -129,7 +129,7 @@ class Editor extends Component {
                     setShowTextEditor={this.props.setShowTextEditor}
                     setMChoiceIndex={this.props.setMChoiceIndex}
                     correctAnswers={this.props.correctAnswers}
-                    setMediaFiles={this.setMediaFiles}
+                    // setMediaFiles={this.setMediaFiles}
                 />
             );
         } else if (this.props.feature === "homePage") {
@@ -145,7 +145,7 @@ class Editor extends Component {
                     setFeatureClass={this.props.setFeatureClass}
                     setShowCssEditor={this.props.setShowCssEditor}
                     resetFeature={this.props.resetFeature}
-                    setMediaFiles={this.setMediaFiles}
+                    // setMediaFiles={this.setMediaFiles}
                 />
             );
         } else if (this.props.feature === "courseObjectives") {
@@ -178,7 +178,7 @@ class Editor extends Component {
                     setShowCssEditor={this.props.setShowCssEditor}
                     setShowTextEditor={this.props.setShowTextEditor}
                     resetFeature={this.props.resetFeature}
-                    setMediaFiles={this.setMediaFiles}
+                    // setMediaFiles={this.setMediaFiles}
                 />
             );
         } else if (this.props.feature === "video") {
@@ -194,7 +194,7 @@ class Editor extends Component {
                     setShowCssEditor={this.props.setShowCssEditor}
                     resetFeature={this.props.resetFeature}
                     setShowEditor={this.props.setShowEditor}
-                    setMediaFiles={this.setMediaFiles}
+                    // setMediaFiles={this.setMediaFiles}
                 />
             );
         } else if (this.props.feature === "dragDrop") {
@@ -229,7 +229,7 @@ class Editor extends Component {
                     resetFeature={this.props.resetFeature}
                     slideItemId={this.props.slideItemId}
                     setShowTextEditor={this.props.setShowTextEditor}
-                    setMediaFiles={this.setMediaFiles}
+                    // setMediaFiles={this.setMediaFiles}
                 />
             );
         } else if (this.props.feature === "image") {
@@ -245,7 +245,7 @@ class Editor extends Component {
                     setShowCssEditor={this.props.setShowCssEditor}
                     resetFeature={this.props.resetFeature}
                     setShowEditor={this.props.setShowEditor}
-                    setMediaFiles={this.setMediaFiles}
+                    // setMediaFiles={this.setMediaFiles}
                 />
             );
         } else if (this.props.feature === "list") {
@@ -262,7 +262,7 @@ class Editor extends Component {
                     setShowCssEditor={this.props.setShowCssEditor}
                     setShowTextEditor={this.props.setShowTextEditor}
                     resetFeature={this.props.resetFeature}
-                    setMediaFiles={this.setMediaFiles}
+                    // setMediaFiles={this.setMediaFiles}
                 />
             );
         } else if (this.props.feature === "tabs") {
@@ -279,7 +279,7 @@ class Editor extends Component {
                     setShowCssEditor={this.props.setShowCssEditor}
                     setShowTextEditor={this.props.setShowTextEditor}
                     resetFeature={this.props.resetFeature}
-                    setMediaFiles={this.setMediaFiles}
+                    // setMediaFiles={this.setMediaFiles}
                     setActiveOutputIndex={this.props.setActiveOutputIndex}
                 />
             );
