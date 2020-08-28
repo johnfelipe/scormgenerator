903c66b1a5b39f72f0efcaba0c0d4beb2fc6b2ad
zeejay09 <zeejaybelamide@gmail.com>
2020-08-28 18:46:59 +0800

    creates coursemeta constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,4 @@ export * from './gallery.constants';
 export * from './lesson.constants';
 export * from './slide.constants';
 export * from './column.constants';
+export * from './coursemeta.constants';
