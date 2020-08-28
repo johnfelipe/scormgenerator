4bb13ff8ce69e6218912ac07374b8515249fa89f
zeejay09 <zeejaybelamide@gmail.com>
2020-08-28 18:50:55 +0800

    creates coursemeta service

---
+++ src/services/coursemeta.service.js
@@ -0,0 +1,45 @@
+import API from "../utils/api";
+
+export const coursemetaService = {
+    createCoursemeta,
+    deleteCoursemeta,
+    getAllCoursemeta,
+    getCoursemeta,
+    getCoursemetaByRkey,
+};
+
+async function createCoursemeta(coursemetaObj) {
+    const response = await API.post('/coursesmeta', coursemetaObj);
+    return handleResponse(response);
+}
+
+async function deleteCoursemeta(id) {
+    const response = await API.delete('/coursesmeta/' + id,);
+    return handleResponse(response);
+}
+
+async function getAllCoursemeta() {
+    const response = await API.get('/coursesmeta',);
+    return handleResponse(response);
+}
+
+async function getCoursemeta(id) {
+    const response = await API.get('/coursesmeta/' + id,);
+    return handleResponse(response);
+}
+
+async function getCoursemetaByRkey(cid, rkey) {
+    const response = await API.get('/course/' + cid + '/coursemeta/rkey/' + rkey,);
+    return handleResponse(response);
+}
+
+function handleResponse(response) {
+    let data = response.data;
+    
+    if (response.status === 500) {
+        const error = response;
+        return Promise.reject(error.message);
+    }
+
+    return data;
+}
