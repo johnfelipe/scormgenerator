2f8b7ab3f9cf5b7b075f0e16f10e6cfc72ec8673
zeejay09 <zeejaybelamide@gmail.com>
2020-09-02 15:27:27 +0800

    adds another condition whether to show the player based on the show property of the audio feature

--- src/components/Slide/Layouts/AudioLayout.js
+++ src/components/Slide/Layouts/AudioLayout.js
@@ -5,19 +5,25 @@ function AudioLayout(props) {
     const { output, style, css } = props;
     
     return (
-        <div id="audio-layout" style={{ backgroundImage: 'url("' + style.backgroundImg.url + '")', backgroundSize: 'cover' }}>
-            <div className="content-area-container h-100 w-100 border border-light p-3 ">
-                {output.audio.url ?
-                    <audio controls>
-                        <source src={output.audio.url} type={output.audio.type}/>
-                        Your browser does not support the audio element.
-                    </audio>
-                :
-                    <span>No audio uploaded.</span>
-                }
-                {props.cssApplier(css, 'audio-layout')}
-            </div>
-        </div>
+        <>
+            {output.audio.show === 'yes' ?
+                <div id="audio-layout" style={{ backgroundImage: 'url("' + style.backgroundImg.url + '")', backgroundSize: 'cover' }}>
+                    <div className="content-area-container h-100 w-100 border border-light p-3 ">
+                        {output.audio.url ?
+                            <audio controls>
+                                <source src={output.audio.url} type={output.audio.type}/>
+                                Your browser does not support the audio element.
+                            </audio>
+                        :
+                            <span>No audio uploaded.</span>
+                        }
+                        {props.cssApplier(css, 'audio-layout')}
+                    </div>
+                </div>
+            :
+                null
+            }
+        </>
     );
 }
 
