61fbeb8ea47d3c07393deec259f587f2d1051812
zeejay09 <zeejaybelamide@gmail.com>
2020-09-02 13:49:17 +0800

    adds condition when audio.url is undefined

--- src/components/Slide/Layouts/AudioLayout.js
+++ src/components/Slide/Layouts/AudioLayout.js
@@ -7,10 +7,14 @@ function AudioLayout(props) {
     return (
         <div id="audio-layout" style={{ backgroundImage: 'url("' + style.backgroundImg.url + '")', backgroundSize: 'cover' }}>
             <div className="content-area-container h-100 w-100 border border-light p-3 ">
-                <audio controls>
-                    <source src={output.audio.url} type={output.audio.type}/>
-                    Your browser does not support the audio element.
-                </audio>
+                {output.audio.url ?
+                    <audio controls>
+                        <source src={output.audio.url} type={output.audio.type}/>
+                        Your browser does not support the audio element.
+                    </audio>
+                :
+                    <span>No audio uploaded.</span>
+                }
                 {props.cssApplier(css, 'audio-layout')}
             </div>
         </div>
