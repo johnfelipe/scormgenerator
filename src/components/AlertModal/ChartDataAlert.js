ec48a0b7ab23d3acf2b1a7002a137be26999e141
zeejay09 <zeejaybelamide@gmail.com>
2020-09-04 16:12:35 +0800

    modifies the alert message and display a sample csv format for the user to follow

--- src/components/AlertModal/ChartDataAlert.js
+++ src/components/AlertModal/ChartDataAlert.js
@@ -9,7 +9,7 @@ function ChartDataAlert(props) {
         <Modal
             show={modalShow}
             onHide={() => props.setModalShow(false)}
-            size="lg"
+            size="md"
             aria-labelledby="contained-modal-title-vcenter"
             centered
             dialogClassName="gallery-preview-modal w-50"
@@ -21,13 +21,41 @@ function ChartDataAlert(props) {
             </Modal.Header>
             <Modal.Body>
                 <span>
-                    CSV Data format is incorrect. Download examples below:
+                    CSV Data format is incorrect. See/Download examples below:
                     <br/>
-                    Example #1:&nbsp;
+                    <span>Example #1:&nbsp;</span>
                     <a href="/assets/sample-csv/temperature-precipitation-data.csv" download>Temperature and Precipitation data through the months</a>
-                    <br/>
-                    Example #2:&nbsp;
+                    <p className="code">
+                        <code>
+                            month,temperature,precipitation<br/>
+                            January,7,8<br/>
+                            February,7,1<br/>
+                            March,10,14.9<br/>
+                            April,15,41<br/>
+                            May,20,31.4<br/>
+                            June,23,42.6<br/>
+                            July,26,57.5<br/>
+                            .<br/>
+                            .<br/>
+                            .<br/>
+                        </code>
+                    </p>
+                    <span>Example #2:&nbsp;</span>
                     <a href="/assets/sample-csv/tennis-players.csv" download>Which tennis player was No.1 the longest?</a>
+                    <p className="code">
+                        <code>
+                            Name,Weeks,Gender<br/>
+                            Steffi Graf,377,Female<br/>
+                            Martina Navratilova,332,Female<br/>
+                            Serena Williams,319,Female<br/>
+                            .<br/>
+                            .<br/>
+                            .<br/>
+                            Evonne Goolagong Cawley,2,Female<br/>
+                            Carlos Moya,2,Male<br/>
+                            Patrick Rafter,1,Male<br/>
+                        </code>
+                    </p>
                 </span>
             </Modal.Body>
         </Modal>
