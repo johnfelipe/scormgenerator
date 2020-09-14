35ccaeaf09a9214341c24d8a4ec20e27e3d635b0
zeejay09 <zeejaybelamide@gmail.com>
2020-09-14 12:30:03 +0800

    creates a webupps dropdown select component

---
+++ src/components/WebuppsComponents/SgDropdownSelect.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function SgDropdownSelect(props) {
+
+    const { selectTitle, currentValue, defaultValue, onChangeHandler, selectId, selectClassName, selectHtmlFor } = props;
+    const selectOptions = props.selectOptions ? props.selectOptions : [];
+
+    return (
+        <div>
+            <label htmlFor={selectHtmlFor} className={"mr-1 " + selectClassName}>{selectTitle}:</label>
+            <select id={selectId} className="form-control d-inline w-50" value={currentValue ? currentValue : defaultValue} onChange={onChangeHandler}>
+                {selectOptions.length > 0 ?
+                    selectOptions.map((option, optionIndex) => (
+                        <option key={'sg-select-dropdown-' + optionIndex} value={option.value}>&nbsp;{option.label}</option>
+                    ))
+                :
+                    <option>&nbsp;No Options added.</option>
+                }
+            </select>
+        </div>
+    );
+}
+
+export default SgDropdownSelect;
