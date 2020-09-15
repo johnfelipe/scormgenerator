4ed0d39706552400998cbe76367129a03309a3f8
zeejay09 <zeejaybelamide@gmail.com>
2020-09-15 16:07:21 +0800

    creates routes for the 2 new pages

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,20 @@ import { Route, Switch } from "react-router-dom";
 // components
 import Home from './containers/Home';
 import Course from './containers/Course';
+import AddSlidePage from './containers/AddSlidePage';
+
+
+import SamplePage from './containers/SamplePage';
 
 function Routes() {
   return (
     <Switch>
         <Route exact path='/' component={ Home } />
         <Route exact path='/course/:cid' component={ Course } />
+        <Route exact path='/course/:cid/lesson/:lid/add-slide' component={ AddSlidePage } />
+
+
+        <Route exact path='/sample' component={ SamplePage } />
     </Switch>
   );
 }
