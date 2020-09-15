63afc09159127420379d9d67a8f67f5b5f599b94
zeejay09 <zeejaybelamide@gmail.com>
2020-09-15 16:06:58 +0800

    creates an add slide page

---
+++ src/containers/AddSlidePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//components
+import SlideHandler from '../components/Handlers/SlideHandler';
+
+function AddSlidePage() {
+
+    return (
+        <div className="container-fluid">
+            <SlideHandler />
+        </div>
+    );
+}
+
+export default AddSlidePage;
