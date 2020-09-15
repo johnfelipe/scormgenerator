e22fdd2ea63785fe01f41da3723934525d676c70
zeejay09 <zeejaybelamide@gmail.com>
2020-09-15 20:02:31 +0800

    adds audio to the preview pane but hides the player

--- src/components/Slide/Layouts/ContentAreaLayout.js
+++ src/components/Slide/Layouts/ContentAreaLayout.js
@@ -10,6 +10,12 @@ function ContentAreaLayout(props) {
             <div className="content-area-container h-100 w-100 border border-light p-3 ">
                 {ReactHtmlParser(output)}
                 {props.cssApplier(css, 'content-area-layout')}
+                {style.backgroundAudio.url &&
+                    <audio controls autoPlay className="d-none">
+                        <source src={style.backgroundAudio.url} type={style.backgroundAudio.type}/>
+                        Your browser does not support the audio element.
+                    </audio>
+                }
             </div>
         </div>
     );
