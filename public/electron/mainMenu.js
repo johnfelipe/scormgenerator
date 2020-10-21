188891b0c8383c4ad17785f1a252b9bad42932ff
zeejay09 <zeejaybelamide@gmail.com>
2020-10-21 15:52:38 +0800

    creates autoUpdater

--- public/electron/mainMenu.js
+++ public/electron/mainMenu.js
@@ -3,8 +3,8 @@ module.exports = [
         label: 'Settings',
         submenu: [
             { label: 'Preferences' },
-            { label: 'Settings 2' },
-            { label: 'Settings 3' },
+            { label: 'About' },
+            { label: 'Help' },
         ]
     },
 ]
