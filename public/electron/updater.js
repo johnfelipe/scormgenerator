5955088de5a1b9486f77e2b0713f4c6a6e4fde8b
zeejay09 <zeejaybelamide@gmail.com>
2020-11-05 17:08:08 +0800

    updates the updater module code to not auto check for updates

--- public/electron/updater.js
+++ public/electron/updater.js
@@ -4,47 +4,71 @@ const { autoUpdater } = require("electron-updater");
 
 autoUpdater.logger = require("electron-log");
 autoUpdater.logger.transports.file.level = "info";
-// autoUpdater.setFeedURL({
-//     provider: 'github',
-//     repo: 'electron-scorm-generator',
-//     owner: 'zeejay09',
-//     // token: process.env.GH_TOKEN,
-//     // private: true,
-// });
-
 autoUpdater.autoDownload = false;
+let updateDownloaded = false;
 
-module.exports = () => {
-    autoUpdater.checkForUpdates();
-    // autoUpdater.checkForUpdatesAndNotify();
+autoUpdater.on('error', (error) => {
+    dialog.showErrorBox('Error during the update', `Application couldn't be updated. Please try again or contact the support team.`);
+});
 
-    autoUpdater.on('update-available', () => {
+autoUpdater.on('update-available', () => {
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'Updates found',
+        message: 'New update is available, do you want update now?',
+        defaultId: 0,
+        cancelId: 1,
+        buttons: ['Yes', 'No']
+    }).then( result => {
+        let buttonIndex = result.response;
+
+        if (buttonIndex === 0) {
+            autoUpdater.downloadUpdate();
+        }
+    });
+});
+autoUpdater.on('update-not-available', () => {
+    dialog.showMessageBox({
+        title: 'No Updates',
+        message: 'Current version is up-to-date.'
+    });
+});
+
+autoUpdater.on('update-downloaded', () => {
+    updateDownloaded = true;
+    dialog.showMessageBox({
+        title: 'Install Updates',
+        message: 'Updates are ready to be installed.',
+        defaultId: 0,
+        cancelId: 1,
+        buttons: ['Install and restart', 'Close']
+    }).then( result => {
+        let buttonIndex = result.response;
+
+        if (buttonIndex === 0) {
+            setImmediate(() => autoUpdater.quitAndInstall());
+        }
+    });
+});
+
+const checkForUpdates = () => {
+    if (updateDownloaded) {
         dialog.showMessageBox({
-            type: 'info',
-            title: 'Update Available',
-            message: 'A new version is available. Do you want to update now?',
-            buttons: ['Update', 'No']
+            title: 'Available Updates',
+            message: 'New updates are available and ready to be installed.',
+            defaultId: 0,
+            cancelId: 1,
+            buttons: ['Install and restart', 'Close']
         }).then( result => {
             let buttonIndex = result.response;
-
+    
             if (buttonIndex === 0) {
-                autoUpdater.downloadUpdate();
+                setImmediate(() => autoUpdater.quitAndInstall());
             }
-        })
-    })
-
-    autoUpdater.on('update-downloaded', () => {
-        dialog.showMessageBox({
-            type: 'info',
-            title: 'Update Ready',
-            message: 'Install and restart now?',
-            buttons: ['Yes', 'Later']
-        }).then( result => {
-            let buttonIndex = result.response;
-
-            if (buttonIndex === 0) {
-                autoUpdater.quitAndInstall(false, true);
-            }
-        })
-    })
+        });
+    } else {
+        autoUpdater.checkForUpdates();
+    }
 }
+
+exports.checkForUpdates = checkForUpdates;
