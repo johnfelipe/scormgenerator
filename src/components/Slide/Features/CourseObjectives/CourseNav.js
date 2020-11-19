08a419091a046bfb902825b7f128b0fbf0bc764c
zeejay09 <zeejaybelamide@gmail.com>
2020-11-19 17:03:29 +0800

    renames the class attribute to className

--- src/components/Slide/Features/CourseObjectives/CourseNav.js
+++ src/components/Slide/Features/CourseObjectives/CourseNav.js
@@ -7,20 +7,20 @@ function CourseNav(props) {
 
     return (
         <div id="course-navigation" className="course-navigation-container h-100 w-100">
-            <div class="container mt-2">
-                <div class="row">
-                    <div class="col-md-3 col-sm-6 item">
-                        <div class="border border-secondary rounded item-card card-block p-3 h-100" style={{ background: props.backgroundColor}}>
+            <div className="container mt-2">
+                <div className="row">
+                    <div className="col-md-3 col-sm-6 item">
+                        <div className="border border-secondary rounded item-card card-block p-3 h-100" style={{ background: props.backgroundColor}}>
                             <div className="w-100 text-center">
                                 <button type="button" className="menu-btn btn btn-light border border-secondary rounded-0">
                                     <FontAwesomeIcon icon={faBars}/>&nbsp;Menu
                                 </button>
                             </div>
-                            <p class="card-text text-center mt-3 text-white">Select to view course slides.</p> 
+                            <p className="card-text text-center mt-3 text-white">Select to view course slides.</p> 
                         </div>
                     </div>
-                    <div class="col-md-3 col-sm-6 item">
-                        <div class="border border-secondary rounded item-card card-block p-3 h-100" style={{ background: props.backgroundColor}}>
+                    <div className="col-md-3 col-sm-6 item">
+                        <div className="border border-secondary rounded item-card card-block p-3 h-100" style={{ background: props.backgroundColor}}>
                             <div className="w-100 text-center">
                                 <button type="button" className="menu-btn btn btn-light border border-secondary rounded-0">
                                     <FontAwesomeIcon icon={faAngleLeft}/>&nbsp;Previous
@@ -29,27 +29,27 @@ function CourseNav(props) {
                                     <FontAwesomeIcon icon={faAngleRight}/>&nbsp;Next
                                 </button>
                             </div>
-                            <p class="card-text text-center mt-3 text-white">Use these buttons to navigate through the slides.</p> 
+                            <p className="card-text text-center mt-3 text-white">Use these buttons to navigate through the slides.</p> 
                         </div>
                     </div>
-                    <div class="col-md-3 col-sm-6 item">
-                        <div class="border border-secondary rounded item-card card-block p-3 h-100" style={{ background: props.backgroundColor}}>
+                    <div className="col-md-3 col-sm-6 item">
+                        <div className="border border-secondary rounded item-card card-block p-3 h-100" style={{ background: props.backgroundColor}}>
                             <div className="w-100 text-center">
                                 <button type="button" className="menu-btn btn btn-light border border-secondary rounded-0">
                                     <FontAwesomeIcon icon={faShareSquare}/>&nbsp;Resources
                                 </button>
                             </div>
-                            <p class="card-text text-center mt-3 text-white">Select to access course source materials.</p> 
+                            <p className="card-text text-center mt-3 text-white">Select to access course source materials.</p> 
                         </div>
                     </div>
-                    <div class="col-md-3 col-sm-6 item">
-                        <div class="border border-secondary rounded item-card card-block p-3 h-100" style={{ background: props.backgroundColor}}>
+                    <div className="col-md-3 col-sm-6 item">
+                        <div className="border border-secondary rounded item-card card-block p-3 h-100" style={{ background: props.backgroundColor}}>
                             <div className="w-100 text-center">
                                 <button type="button" className="menu-btn btn btn-light border border-secondary rounded-0">
                                     <FontAwesomeIcon icon={faBook}/>&nbsp;Glossary
                                 </button>
                             </div>
-                            <p class="card-text text-center mt-3 text-white">Use the glossary for definitions, acronyms, and abbreviations.</p> 
+                            <p className="card-text text-center mt-3 text-white">Use the glossary for definitions, acronyms, and abbreviations.</p> 
                         </div>
                     </div>    
                 </div>
