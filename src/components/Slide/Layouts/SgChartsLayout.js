e512f05f2555fdc13af5be9c3cd434f76c4f4e80
zeejay09 <zeejaybelamide@gmail.com>
2020-11-19 18:20:50 +0800

    customizes the width and height of the chart to fit into the fixed height

--- src/components/Slide/Layouts/SgChartsLayout.js
+++ src/components/Slide/Layouts/SgChartsLayout.js
@@ -5,7 +5,7 @@ import ReactHtmlParser from 'react-html-parser';
 function SgChartsLayout(props) {
 
     // const { output, css, style } = props;
-    const { output } = props;
+    const { output, style, slideName, slideSubtitle, showTitle } = props;
     let datasets = [];
 
     if (output.dataSets.data) {
@@ -33,7 +33,12 @@ function SgChartsLayout(props) {
                 return (
                     <>
                         <div id="sg-pie-chart">
-                            <Pie data={data} />
+                            <Pie
+                                data={data}
+                                width={100}
+                                height={225}
+                                options={{ maintainAspectRatio: false }}
+                            />
                             <label className="mt-1"><strong>{output.label}</strong></label>
                         </div>
                         <div id="sg-chart-description" className="webupps-text-indent-5 text-justify">
@@ -45,7 +50,12 @@ function SgChartsLayout(props) {
                 return (
                     <>
                         <div id="sg-doughnut-chart">
-                            <Doughnut data={data} />
+                            <Doughnut
+                                data={data}
+                                width={100}
+                                height={225}
+                                options={{ maintainAspectRatio: false }}
+                            />
                             <label className="mt-1"><strong>{output.label}</strong></label>
                         </div>
                         <div id="sg-chart-description" className="webupps-text-indent-5 text-justify">
@@ -57,7 +67,12 @@ function SgChartsLayout(props) {
                 return (
                     <>
                         <div id="sg-bar-chart">
-                            <Bar data={data} />
+                            <Bar
+                                data={data}
+                                width={100}
+                                height={225}
+                                options={{ maintainAspectRatio: false }}
+                            />
                             <label className="mt-1"><strong>{output.label}</strong></label>
                         </div>
                         <div id="sg-chart-description" className="webupps-text-indent-5 text-justify">
@@ -74,8 +89,44 @@ function SgChartsLayout(props) {
     };
 
     return (
-        <div id="sg-charts-layout" className="w-100 h-100 p-3 border border-light">
-            {chartLayoutContent()}
+        <div className="sg-charts-layout">
+            <div className="w-100 h-100 p-4">
+                <div className={showTitle ? "row" : "row d-none"}>
+                    <div className="col-12">
+                        {style.titleBoxBorder === 'border-left' ?
+                            <div
+                                className={"slide-header text-left " + style.titleBoxBorder}
+                                ref={(el) => {
+                                    if (el) {
+                                        el.style.setProperty('border-left-color', style.titleBoxColor, 'important');
+                                        el.style.setProperty('color', style.titleTextColor, 'important');
+                                    }
+                                }}
+                            >
+                                <h3 className="slide-subtitle">{slideName}</h3>
+                                <h2 class="slide-title">{slideSubtitle}</h2>
+                            </div>
+                        :
+                            <div className="slide-header text-left">
+                                <h3 className="slide-subtitle">{slideName}</h3>
+                                <h2 className="slide-title">
+                                    <span
+                                        className={style.titleBoxBorder}
+                                        ref={(el) => {
+                                            if (el) {
+                                                el.style.setProperty('border-top-color', style.titleBoxColor, 'important');
+                                            }
+                                        }}
+                                    >
+                                        {slideSubtitle}
+                                    </span>
+                                </h2>
+                            </div>
+                        }
+                    </div>
+                </div>
+                {chartLayoutContent()}
+            </div>
         </div>
     );
 }
