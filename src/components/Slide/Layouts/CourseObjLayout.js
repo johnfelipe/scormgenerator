e5da63e9a2d58218373b2d83f55e84c30fd9a13c
zeejay09 <zeejaybelamide@gmail.com>
2020-11-19 17:03:47 +0800

    creates a normal modal html alongside with the react bootstrap modal

--- src/components/Slide/Layouts/CourseObjLayout.js
+++ src/components/Slide/Layouts/CourseObjLayout.js
@@ -33,7 +33,7 @@ function CourseObjLayout(props) {
         >
             <Modal.Header closeButton>
                 <Modal.Title>
-                    Course Navigation
+                    {courseNavTitle}
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
@@ -106,7 +106,7 @@ function CourseObjLayout(props) {
                                     }}
                                 >
                                     <h3 className="slide-subtitle">{slideName}</h3>
-                                    <h2 class="slide-title">{slideSubtitle}</h2>
+                                    <h2 className="slide-title">{slideSubtitle}</h2>
                                 </div>
                             :
                                 <div className="slide-header text-left">
@@ -141,14 +141,26 @@ function CourseObjLayout(props) {
                         <div className="col-md-4">
                             <div className="course-intro-wrapper w-100 h-100 p-4" style={{ background: styles.courseIntroColor}}>
                                 <div className="mt-3">
-                                    <button type="button" className="btn btn-light rounded-0 mt-3 w-100 font-14" onClick={() => setCourseNavModalShow(true)}>
+                                    {/* <button type="button" className="btn btn-light rounded-0 mt-3 w-100 font-14" onClick={() => setCourseNavModalShow(true)}>
                                         <FontAwesomeIcon icon={faInfoCircle}/>&nbsp;{courseNavTitle}
+                                    </button> */}
+                                    <button type="button" className="btn btn-light rounded-0 mt-3 w-100 font-14" data-toggle="modal" data-target="#courseNav" onClick={() => setCourseNavModalShow(true)}>
+                                        <i className="fas fa-info-circle"></i>
+                                        &nbsp;{courseNavTitle}
                                     </button>
-                                    <button type="button" className="btn btn-light rounded-0 mt-3 w-100 font-14" onClick={() => setCourseInfoModalShow(true)}>
+                                    {/* <button type="button" className="btn btn-light rounded-0 mt-3 w-100 font-14" onClick={() => setCourseInfoModalShow(true)}>
                                         <FontAwesomeIcon icon={faInfoCircle}/>&nbsp;{courseInfoTitle}
+                                    </button> */}
+                                    <button type="button" className="btn btn-light rounded-0 mt-3 w-100 font-14" data-toggle="modal" data-target="#courseInfo"  onClick={() => setCourseInfoModalShow(true)}>
+                                        <i className="fas fa-info-circle"></i>
+                                        &nbsp;{courseInfoTitle}
                                     </button>
-                                    <button type="button" className="btn btn-light rounded-0 mt-3 w-100 font-14" onClick={() => setCourseReqModalShow(true)}>
+                                    {/* <button type="button" className="btn btn-light rounded-0 mt-3 w-100 font-14" onClick={() => setCourseReqModalShow(true)}>
                                         <FontAwesomeIcon icon={faInfoCircle}/>&nbsp;{courseReqTitle}
+                                    </button> */}
+                                    <button type="button" className="btn btn-light rounded-0 mt-3 w-100 font-14" data-toggle="modal" data-target="#courseReq" onClick={() => setCourseReqModalShow(true)}>
+                                        <i className="fas fa-info-circle"></i>
+                                        &nbsp;{courseReqTitle}
                                     </button>
                                 </div>
                             </div>
@@ -172,7 +184,7 @@ function CourseObjLayout(props) {
                                     }}
                                 >
                                     <h3 className="slide-subtitle">{slideName}</h3>
-                                    <h2 class="slide-title">{slideSubtitle}</h2>
+                                    <h2 className="slide-title">{slideSubtitle}</h2>
                                 </div>
                             :
                                 <div className="slide-header text-left">
@@ -197,14 +209,26 @@ function CourseObjLayout(props) {
                         <div className="col-md-4">
                             <div className="course-intro-wrapper w-100 h-100 p-4" style={{ background: styles.courseIntroColor}}>
                                 <div className="mt-3">
-                                    <button type="button" className="btn btn-light rounded-0 mt-3 w-100 font-14" onClick={() => setCourseNavModalShow(true)}>
+                                    {/* <button type="button" className="btn btn-light rounded-0 mt-3 w-100 font-14" onClick={() => setCourseNavModalShow(true)}>
                                         <FontAwesomeIcon icon={faInfoCircle}/>&nbsp;{courseNavTitle}
+                                    </button> */}
+                                    <button type="button" className="btn btn-light rounded-0 mt-3 w-100 font-14" data-toggle="modal" data-target="#courseNav">
+                                        <i className="fas fa-info-circle"></i>
+                                        &nbsp;{courseNavTitle}
                                     </button>
-                                    <button type="button" className="btn btn-light rounded-0 mt-3 w-100 font-14" onClick={() => setCourseInfoModalShow(true)}>
+                                    {/* <button type="button" className="btn btn-light rounded-0 mt-3 w-100 font-14" onClick={() => setCourseInfoModalShow(true)}>
                                         <FontAwesomeIcon icon={faInfoCircle}/>&nbsp;{courseInfoTitle}
+                                    </button> */}
+                                    <button type="button" className="btn btn-light rounded-0 mt-3 w-100 font-14" data-toggle="modal" data-target="#courseInfo">
+                                        <i className="fas fa-info-circle"></i>
+                                        &nbsp;{courseInfoTitle}
                                     </button>
-                                    <button type="button" className="btn btn-light rounded-0 mt-3 w-100 font-14" onClick={() => setCourseReqModalShow(true)}>
+                                    {/* <button type="button" className="btn btn-light rounded-0 mt-3 w-100 font-14" onClick={() => setCourseReqModalShow(true)}>
                                         <FontAwesomeIcon icon={faInfoCircle}/>&nbsp;{courseReqTitle}
+                                    </button> */}
+                                    <button type="button" className="btn btn-light rounded-0 mt-3 w-100 font-14" data-toggle="modal" data-target="#courseReq">
+                                        <i className="fas fa-info-circle"></i>
+                                        &nbsp;{courseReqTitle}
                                     </button>
                                 </div>
                             </div>
@@ -233,6 +257,60 @@ function CourseObjLayout(props) {
             {courseNavModal}
             {courseInfoModal}
             {courseReqModal}
+            
+            {/* Modals */}
+            {/* Course Nav */}
+            <div role="dialog" aria-hidden="true" className="fade modal" tabIndex="-1" aria-labelledby="courseNavLabel" id="courseNav">
+                <div className="modal-dialog modal-lg modal-dialog-centered" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <div className="modal-title h4">{courseNavTitle}</div>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <CourseNavContent 
+                                backgroundColor={styles.courseIntroColor}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Course info */}
+            <div role="dialog" aria-hidden="true" className="fade modal" tabIndex="-1" aria-labelledby="courseInfoLabel" id="courseInfo">
+                <div role="document" className="modal-dialog modal-lg modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <div className="modal-title h4">{courseInfoTitle}</div>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            {ReactHtmlParser(courseInfoContent)}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Course req */}
+            <div role="dialog" aria-hidden="true" className="fade modal" tabIndex="-1" aria-labelledby="courseReqLabel" id="courseReq">
+                <div role="document" className="modal-dialog modal-lg modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <div className="modal-title h4">{courseReqTitle}</div>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            {ReactHtmlParser(courseReqContent)}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
